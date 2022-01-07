cd5242bfc4db0242281db66c5d75053fef6f9697
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-01-07 12:14:38 -0500

    example: remove unncessary cfg

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -2,7 +2,7 @@ use dioxus::prelude::*;
 use dioxus_desktop::tao::menu::MenuBar;
 
 fn main() {
-    dioxus::desktop::launch_cfg(app, |c| c.with_window(|w| w.with_menu(MenuBar::default())));
+    dioxus::desktop::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
