71656adc897dc1e3fb50010e10b3488059d12dfe
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-02-01 15:54:32 -0500

    publish: generate our bindings.rs file from an inlinejs snippet

--- packages/interpreter/build.rs
+++ packages/interpreter/build.rs
@@ -2,5 +2,14 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=interpreter.ts");
-    Command::new("tsc").spawn().unwrap();
+    match Command::new("tsc").spawn() {
+        Ok(_) => println!("Was spawned :)"),
+        Err(e) => {
+            if let std::io::ErrorKind::NotFound = e.kind() {
+                println!("`tsc` was not found! Not going to generate new interpreter")
+            } else {
+                println!("Some strange error occurred :(");
+            }
+        }
+    }
 }
