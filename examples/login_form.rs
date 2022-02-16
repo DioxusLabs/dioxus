4d965c8571774c59c5c6d6a3223a747301da4949
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-02-16 14:16:03 -0500

    feat: simplify login example

--- examples/login_form.rs
+++ examples/login_form.rs
@@ -10,39 +10,22 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    cx.render(rsx! {
-        Router {
-            Route { to: "/", home() }
-            Route { to: "/login", login() }
-        }
-    })
-}
-
-fn home(cx: Scope) -> Element {
-    cx.render(rsx! {
-        h1 { "Welcome Home" }
-        Link { to: "/login", "Login" }
-    })
-}
-
-fn login(cx: Scope) -> Element {
-    let service = use_router(&cx);
-
     let onsubmit = move |evt: FormEvent| {
-        to_owned![service];
-        let username = evt.values["username"].clone();
-        let password = evt.values["password"].clone();
-
         cx.spawn(async move {
             let resp = reqwest::Client::new()
                 .post("http://localhost/login")
-                .form(&[("username", username), ("password", password)])
+                .form(&[
+                    ("username", &evt.values["username"]),
+                    ("password", &evt.values["password"]),
+                ])
                 .send()
                 .await;
 
             match resp {
                 // Parse data from here, such as storing a response token
-                Ok(_data) => service.push_route("/"),
+                Ok(_data) => {
+                    println!("Login successful");
+                }
 
                 //Handle any errors from the fetch here
                 Err(_err) => {}
