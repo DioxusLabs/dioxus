e7022dfccf64b378ac08a93c02758690934821fc
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-02-19 21:51:26 -0500

    docs: more use_ref docs

--- examples/use_ref.rs
+++ examples/use_ref.rs
@@ -6,12 +6,16 @@ fn main() {}
 fn app(cx: Scope) -> Element {
     let val = use_ref(&cx, || HashMap::<u32, String>::new());
 
-    // Pull the value out locally
-    let p = val.read();
+    {
+        // Pull the value out locally
+        let p = val.read();
 
-    // to get an &HashMap we have to "reborrow" through the RefCell
-    // Be careful: passing this into children might cause a double borrow of the RefCell and a panic
-    let g = &*p;
+        // to get an &HashMap we have to "reborrow" through the RefCell
+        // Be careful: passing this into children might cause a double borrow of the RefCell and a panic
+        let g = &*p;
+
+        dbg!(g);
+    }
 
     cx.render(rsx! {
         div {
