11168b3180ecba2d7dd5cf06c447eabbe513d922
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-03-03 11:43:49 +0100

    setup channel

--- examples/bevy.rs
+++ examples/bevy.rs
@@ -1,21 +1,53 @@
 use bevy::prelude::*;
-use dioxus::desktop::{DioxusDesktopContext, DioxusDesktopPlugin};
+use dioxus::desktop::{AppProps, DioxusDesktop, DioxusDesktopPlugin};
 use dioxus::prelude::*;
+use std::marker::PhantomData;
+
+#[derive(Debug)]
+enum CoreCommand {
+    Click,
+}
+
+#[derive(Clone, Debug)]
+enum UICommand {
+    Test,
+}
 
 fn main() {
     App::new()
-        .insert_resource(DioxusDesktopContext {
+        .add_plugin(DioxusDesktopPlugin::<CoreCommand, UICommand> {
             root: app,
-            props: (),
+            core_cmd_type: PhantomData,
+            ui_cmd_type: PhantomData,
         })
-        .add_plugin(DioxusDesktopPlugin)
+        .add_startup_system(setup)
         .run();
 }
 
-fn app(cx: Scope) -> Element {
+fn setup(desktop: Res<DioxusDesktop<CoreCommand, UICommand>>) {
+    println!("setup");
+    let _res = desktop.sender().send(UICommand::Test);
+}
+
+fn app(cx: Scope<AppProps<CoreCommand, UICommand>>) -> Element {
+    use_future(&cx, || {
+        let mut rx = cx.props.channel.1.subscribe();
+        async move {
+            while let Ok(cmd) = rx.recv().await {
+                println!("🎨 {:?}", cmd);
+            }
+        }
+    });
+
     cx.render(rsx! {
         div {
-            h1 { "Bevy Plugin Example" }
+            h1 { "Bevy Plugin Example" },
+            button {
+                onclick: |_e| {
+                    let _res = cx.props.channel.0.send(CoreCommand::Click);
+                },
+                "Send",
+            }
         }
     })
 }
--- packages/desktop/src/lib.rs
+++ packages/desktop/src/lib.rs
@@ -25,8 +25,12 @@ pub use wry;
 pub use wry::application as tao;
 use wry::webview::WebViewBuilder;
 
-pub use bevy::prelude::Component as BevyComponent;
-pub use bevy::prelude::*;
+pub use bevy::prelude::{Component as BevyComponent, *};
+pub use std::{fmt::Debug, marker::PhantomData};
+pub use tokio::sync::{
+    broadcast::{channel, Receiver, Sender},
+    mpsc,
+};
 
 use crate::events::trigger_from_serialized;
 
@@ -229,99 +233,154 @@ pub fn launch_with_props<P: 'static + Send>(
     })
 }
 
-pub struct DioxusDesktopPlugin;
+pub struct DioxusDesktopPlugin<CoreCommand, UICommand> {
+    pub root: Component<AppProps<CoreCommand, UICommand>>,
+    pub core_cmd_type: PhantomData<CoreCommand>,
+    pub ui_cmd_type: PhantomData<UICommand>,
+}
 
-impl Plugin for DioxusDesktopPlugin {
+impl<CoreCommand: 'static + Send + Sync + Debug, UICommand: 'static + Send + Sync + Clone> Plugin
+    for DioxusDesktopPlugin<CoreCommand, UICommand>
+{
     fn build(&self, app: &mut App) {
-        app.set_runner(runner);
+        app.insert_resource(DioxusDesktop {
+            root: self.root,
+            sender: None,
+        })
+        .set_runner(|app| DioxusDesktop::<CoreCommand, UICommand>::runner(app));
     }
 }
 
-pub struct DioxusDesktopContext<P: 'static + Send = ()> {
-    pub root: Component<P>,
-    pub props: P,
+pub struct DioxusDesktop<CoreCommand, UICommand> {
+    root: Component<AppProps<CoreCommand, UICommand>>,
+    sender: Option<Sender<UICommand>>,
 }
 
-fn runner(app: App) {
-    let mut cfg = DesktopConfig::default().with_default_icon();
-    // builder(&mut cfg);
-    let event_loop = EventLoop::with_user_event();
-    let ctx = app
-        .world
-        .get_resource::<DioxusDesktopContext>()
-        .expect("Provide DioxusDesktopContext resource");
+impl<CoreCommand, UICommand> DioxusDesktop<CoreCommand, UICommand> {
+    pub fn sender(&self) -> Sender<UICommand> {
+        self.sender
+            .clone()
+            .expect("Sender<UICommand> isn't initialized")
+    }
+}
 
-    let mut desktop =
-        DesktopController::new_on_tokio(ctx.root, ctx.props, event_loop.create_proxy());
-    let proxy = event_loop.create_proxy();
+impl<CoreCommand, UICommand> DioxusDesktop<CoreCommand, UICommand> {
+    fn set_sender(&mut self, sender: Sender<UICommand>) {
+        self.sender = Some(sender);
+    }
+}
 
-    event_loop.run(move |window_event, event_loop, control_flow| {
-        *control_flow = ControlFlow::Wait;
+pub struct AppProps<CoreCommand, UICommand> {
+    pub channel: (mpsc::UnboundedSender<CoreCommand>, Sender<UICommand>),
+}
 
-        match window_event {
-            Event::NewEvents(StartCause::Init) => {
-                let builder = cfg.window.clone();
+impl<CoreCommand: 'static + Send + Debug, UICommand: 'static + Send + Clone>
+    DioxusDesktop<CoreCommand, UICommand>
+{
+    fn runner(mut app: App) {
+        let mut cfg = DesktopConfig::default().with_default_icon();
+        // builder(&mut cfg);
+        let event_loop = EventLoop::with_user_event();
 
-                let window = builder.build(event_loop).unwrap();
-                let window_id = window.id();
+        let (core_tx, mut core_rx) = mpsc::unbounded_channel::<CoreCommand>();
+        let (ui_tx, _) = channel::<UICommand>(8);
 
-                let (is_ready, sender) = (desktop.is_ready.clone(), desktop.sender.clone());
+        let mut desktop_resource = app
+            .world
+            .get_resource_mut::<DioxusDesktop<CoreCommand, UICommand>>()
+            .expect("Provide DioxusDesktopConfig resource");
 
-                let proxy = proxy.clone();
-                let file_handler = cfg.file_drop_handler.take();
+        desktop_resource.set_sender(ui_tx.clone());
 
-                let mut webview = WebViewBuilder::new(window)
-                    .unwrap()
-                    .with_transparent(cfg.window.window.transparent)
-                    .with_url("dioxus://index.html/")
-                    .unwrap()
-                    .with_ipc_handler(move |_window: &Window, payload: String| {
-                        parse_ipc_message(&payload)
-                            .map(|message| match message.method() {
-                                "user_event" => {
-                                    let event = trigger_from_serialized(message.params());
-                                    log::trace!("User event: {:?}", event);
-                                    sender.unbounded_send(SchedulerMsg::Event(event)).unwrap();
-                                }
-                                "initialize" => {
-                                    is_ready.store(true, std::sync::atomic::Ordering::Relaxed);
-                                    let _ = proxy
-                                        .send_event(user_window_events::UserWindowEvent::Update);
-                                }
-                                "browser_open" => {
-                                    let data = message.params();
-                                    log::trace!("Open browser: {:?}", data);
-                                    if let Some(temp) = data.as_object() {
-                                        if temp.contains_key("href") {
-                                            let url = temp.get("href").unwrap().as_str().unwrap();
-                                            if let Err(e) = webbrowser::open(url) {
-                                                log::error!("Open Browser error: {:?}", e);
+        let props = AppProps::<CoreCommand, UICommand> {
+            channel: (core_tx, ui_tx),
+        };
+
+        let mut desktop = DesktopController::new_on_tokio::<AppProps<CoreCommand, UICommand>>(
+            desktop_resource.root,
+            props,
+            event_loop.create_proxy(),
+        );
+        let proxy = event_loop.create_proxy();
+
+        let runtime = tokio::runtime::Runtime::new().expect("Failed to initialize runtime");
+
+        runtime.spawn(async move {
+            while let Some(cmd) = core_rx.recv().await {
+                println!("🧠 {:?}", cmd);
+            }
+        });
+
+        app.run();
+
+        event_loop.run(move |window_event, event_loop, control_flow| {
+            *control_flow = ControlFlow::Wait;
+
+            match window_event {
+                Event::NewEvents(StartCause::Init) => {
+                    let builder = cfg.window.clone();
+
+                    let window = builder.build(event_loop).unwrap();
+                    let window_id = window.id();
+
+                    let (is_ready, sender) = (desktop.is_ready.clone(), desktop.sender.clone());
+
+                    let proxy = proxy.clone();
+                    let file_handler = cfg.file_drop_handler.take();
+
+                    let mut webview = WebViewBuilder::new(window)
+                        .unwrap()
+                        .with_transparent(cfg.window.window.transparent)
+                        .with_url("dioxus://index.html/")
+                        .unwrap()
+                        .with_ipc_handler(move |_window: &Window, payload: String| {
+                            parse_ipc_message(&payload)
+                                .map(|message| match message.method() {
+                                    "user_event" => {
+                                        let event = trigger_from_serialized(message.params());
+                                        sender.unbounded_send(SchedulerMsg::Event(event)).unwrap();
+                                    }
+                                    "initialize" => {
+                                        is_ready.store(true, std::sync::atomic::Ordering::Relaxed);
+                                        let _ = proxy.send_event(
+                                            user_window_events::UserWindowEvent::Update,
+                                        );
+                                    }
+                                    "browser_open" => {
+                                        let data = message.params();
+                                        log::trace!("Open browser: {:?}", data);
+                                        if let Some(temp) = data.as_object() {
+                                            if temp.contains_key("href") {
+                                                let url =
+                                                    temp.get("href").unwrap().as_str().unwrap();
+                                                if let Err(e) = webbrowser::open(url) {
+                                                    log::error!("Open Browser error: {:?}", e);
+                                                }
                                             }
                                         }
                                     }
-                                }
-                                _ => (),
-                            })
-                            .unwrap_or_else(|| {
-                                log::warn!("invalid IPC message received");
-                            });
-                    })
-                    .with_custom_protocol(String::from("dioxus"), protocol::desktop_handler)
-                    .with_file_drop_handler(move |window, evet| {
-                        file_handler
-                            .as_ref()
-                            .map(|handler| handler(window, evet))
-                            .unwrap_or_default()
-                    });
+                                    _ => (),
+                                })
+                                .unwrap_or_else(|| {
+                                    log::warn!("invalid IPC message received");
+                                })
+                        })
+                        .with_custom_protocol(String::from("dioxus"), protocol::desktop_handler)
+                        .with_file_drop_handler(move |window, evet| {
+                            file_handler
+                                .as_ref()
+                                .map(|handler| handler(window, evet))
+                                .unwrap_or_default()
+                        });
 
-                for (name, handler) in cfg.protocols.drain(..) {
-                    webview = webview.with_custom_protocol(name, handler)
-                }
+                    for (name, handler) in cfg.protocols.drain(..) {
+                        webview = webview.with_custom_protocol(name, handler)
+                    }
 
-                if cfg.disable_context_menu {
-                    // in release mode, we don't want to show the dev tool or reload menus
-                    webview = webview.with_initialization_script(
-                        r#"
+                    if cfg.disable_context_menu {
+                        // in release mode, we don't want to show the dev tool or reload menus
+                        webview = webview.with_initialization_script(
+                            r#"
                         if (document.addEventListener) {
                         document.addEventListener('contextmenu', function(e) {
                             alert("You've tried to open context menu");
@@ -334,39 +393,40 @@ fn runner(app: App) {
                         });
                         }
                     "#,
-                    )
-                } else {
-                    // in debug, we are okay with the reload menu showing and dev tool
-                    webview = webview.with_dev_tool(true);
-                }
-
-                desktop.webviews.insert(window_id, webview.build().unwrap());
-            }
-
-            Event::WindowEvent {
-                event, window_id, ..
-            } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Destroyed { .. } => desktop.close_window(window_id, control_flow),
-
-                WindowEvent::Resized(_) | WindowEvent::Moved(_) => {
-                    if let Some(view) = desktop.webviews.get_mut(&window_id) {
-                        let _ = view.resize();
+                        )
+                    } else {
+                        // in debug, we are okay with the reload menu showing and dev tool
+                        webview = webview.with_dev_tool(true);
                     }
+
+                    desktop.webviews.insert(window_id, webview.build().unwrap());
                 }
 
-                _ => {}
-            },
+                Event::WindowEvent {
+                    event, window_id, ..
+                } => match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Destroyed { .. } => desktop.close_window(window_id, control_flow),
 
-            Event::UserEvent(user_event) => {
-                user_window_events::handler(user_event, &mut desktop, control_flow)
+                    WindowEvent::Resized(_) | WindowEvent::Moved(_) => {
+                        if let Some(view) = desktop.webviews.get_mut(&window_id) {
+                            let _ = view.resize();
+                        }
+                    }
+
+                    _ => {}
+                },
+
+                Event::UserEvent(user_event) => {
+                    user_window_events::handler(user_event, &mut desktop, control_flow)
+                }
+                Event::MainEventsCleared => {}
+                Event::Resumed => {}
+                Event::Suspended => {}
+                Event::LoopDestroyed => {}
+                Event::RedrawRequested(_id) => {}
+                _ => {}
             }
-            Event::MainEventsCleared => {}
-            Event::Resumed => {}
-            Event::Suspended => {}
-            Event::LoopDestroyed => {}
-            Event::RedrawRequested(_id) => {}
-            _ => {}
-        }
-    })
+        })
+    }
 }
