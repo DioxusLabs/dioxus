83be3df42a265d24fc8fa63fb6a7acff54c40221
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-03-04 14:34:25 -0500

    cfg: expose router feature flags for web

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,6 @@ pub use dioxus_desktop as desktop;
 #[cfg(feature = "fermi")]
 pub use fermi;
 
-// #[cfg(feature = "mobile")]
-// pub use dioxus_mobile as mobile;
-
 pub mod events {
     #[cfg(feature = "html")]
     pub use dioxus_html::{on::*, KeyCode};
@@ -34,4 +31,10 @@ pub mod prelude {
     pub use dioxus_elements::{GlobalAttributes, SvgAttributes};
     pub use dioxus_hooks::*;
     pub use dioxus_html as dioxus_elements;
+
+    #[cfg(feature = "router")]
+    pub use dioxus_router::{use_route, use_router, Link, Redirect, Router, UseRoute};
+
+    #[cfg(feature = "fermi")]
+    pub use fermi::{use_atom_ref, use_init_atom_root, use_read, use_set, Atom, AtomRef};
 }
