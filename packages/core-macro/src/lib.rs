794f43ffee84c2b838d9f895912c00ebe52fa957
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-04-24 02:35:52 -0400

    concept: move rsx out to its own crate

--- packages/core-macro/src/lib.rs
+++ packages/core-macro/src/lib.rs
@@ -5,7 +5,9 @@ use syn::parse_macro_input;
 mod ifmt;
 mod inlineprops;
 mod props;
-mod rsx;
+use dioxus_rsx as rsx;
+
+// mod rsx;
 
 #[proc_macro]
 pub fn format_args_f(input: TokenStream) -> TokenStream {
