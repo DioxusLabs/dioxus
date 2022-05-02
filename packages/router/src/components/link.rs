bd14a0240e4a109e582d5629f8060a915e1dc772
Adrian Wannenmacher <tfld@tfld.dev>
2022-05-02 19:34:10 +0200

    implement class, id and new_tab for link and fix active route detection for relative paths

--- packages/router/src/components/link.rs
+++ packages/router/src/components/link.rs
@@ -5,18 +5,29 @@ use log::error;
 
 use crate::{
     helpers::{construct_named_path, sub_to_router},
-    navigation::NavigationTarget,
+    navigation::NavigationTarget::{self, *},
     service::RouterMessage,
 };
 
 /// The properties for a [`Link`].
 #[derive(Props)]
 pub struct LinkProps<'a> {
-    /// A class to apply to the generated `a` tag when the link is active.
+    /// A class to apply to the inner `a` tag when the link is active.
     pub active_class: Option<&'a str>,
     /// The children to render within the [`Link`].
     pub children: Element<'a>,
-    /// The `rel` attribute of the rendered `a` tag.
+    /// The classes of the inner `a` tag.
+    ///
+    /// When the link is active and an `active_class` is provided, it is appended at the end.
+    pub class: Option<&'a str>,
+    /// An ID of the inner `a` tag.
+    pub id: Option<&'a str>,
+    /// Specify whether the link should be opened in a new tab.
+    #[props(default)]
+    pub new_tab: bool,
+    /// The `rel` attribute of the inner `a` tag.
+    ///
+    /// Defaults to `"noreferrer noopener"` for [`NtExternal`] targets.
     pub rel: Option<&'a str>,
     /// The navigation target. Corresponds to the `href` of an `a` tag.
     pub target: NavigationTarget,
@@ -36,6 +47,9 @@ pub fn Link<'a>(cx: Scope<'a, LinkProps<'a>>) -> Element {
     let LinkProps {
         active_class,
         children,
+        class,
+        id,
+        new_tab,
         rel,
         target,
     } = cx.props;
@@ -51,34 +65,50 @@ pub fn Link<'a>(cx: Scope<'a, LinkProps<'a>>) -> Element {
     let state = router.state.read().expect("router lock poison");
     let tx = router.tx.clone();
 
+    // generate href
+    let href = match target {
+        NtPath(path) | NavigationTarget::NtExternal(path) => path.to_string(),
+        NtName(name, vars) => construct_named_path(name, vars, &router.named_routes)
+            .unwrap_or(String::from("invalid path")),
+    };
+
     // check if route is active
     let active_class = active_class
         .map(|ac| ac.to_string())
-        .or(router.active_class.clone());
-    let mut active = String::new();
-    if let Some(ac) = active_class {
-        match target {
-            NavigationTarget::NtPath(p) => {
-                if state.path.starts_with(p) {
-                    active = ac;
-                }
-            }
-            NavigationTarget::NtName(n, _) => {
-                if state.names.contains(n) {
-                    active = ac
-                }
-            }
-            NavigationTarget::NtExternal(_) => { /* do nothing */ }
-        }
-    }
+        .or(router.active_class.clone())
+        .map(|ac| {
+            match target {
+                NtPath(p) => {
+                    if p.starts_with("/") && state.path.starts_with(p) {
+                        return format!(" {ac}");
+                    }
 
-    // generate href
-    let href = match target {
-        NavigationTarget::NtPath(path) | NavigationTarget::NtExternal(path) => path.to_string(),
-        NavigationTarget::NtName(name, vars) => {
-            construct_named_path(name, vars, &router.named_routes)
-                .unwrap_or(String::from("invalid path"))
-        }
+                    if !state.path.ends_with("/") {
+                        if let Some(path) = state.path.split("/").last() {
+                            if p == path {
+                                return format!(" {ac}");
+                            }
+                        }
+                    }
+                }
+                NtName(n, _) => {
+                    if state.names.contains(n) {
+                        return format!(" {ac}");
+                    }
+                }
+                NtExternal(_) => {}
+            }
+            String::new()
+        })
+        .unwrap_or_default();
+
+    // prepare id, class and target for the `a` tag
+    let id = id.unwrap_or_default();
+    let class = class.unwrap_or_default();
+    let class = format!("{class}{active_class}");
+    let tag_target = match new_tab {
+        true => "_blank",
+        false => "",
     };
 
     // prepare prevented defaults
@@ -99,14 +129,16 @@ pub fn Link<'a>(cx: Scope<'a, LinkProps<'a>>) -> Element {
     cx.render(rsx! {
         a {
             href: "{href}",
-            class: "{active}",
             prevent_default: "{prevent}",
             onclick: move |_| {
                 if !target.is_nt_external() {
                     tx.unbounded_send(RouterMessage::Push(target.clone().into())).ok();
                 }
             },
+            class: "{class}",
+            id: "{id}",
             rel: "{rel}",
+            target: "{tag_target}",
             children
         }
     })
