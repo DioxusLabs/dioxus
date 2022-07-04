e6657b4bf0cf77cffd8fec53cc6f3002acd5721c
Reinis Mazeiks <reinis.mazeiks@gmail.com>
2022-07-04 22:28:36 +0300

    Shorten `use_future` guide

--- docs/guide/examples/use_future.rs
+++ docs/guide/examples/use_future.rs
@@ -13,6 +13,7 @@ struct ApiResponse {
 }
 
 fn App(cx: Scope) -> Element {
+    // ANCHOR: use_future
     let future = use_future(&cx, (), |_| async move {
         reqwest::get("https://dog.ceo/api/breeds/image/random")
             .await
@@ -20,7 +21,9 @@ fn App(cx: Scope) -> Element {
             .json::<ApiResponse>()
             .await
     });
+    // ANCHOR_END: use_future
 
+    // ANCHOR: render
     cx.render(match future.value() {
         Some(Ok(response)) => rsx! {
             button {
@@ -38,4 +41,20 @@ fn App(cx: Scope) -> Element {
         Some(Err(_)) => rsx! { div { "Loading dogs failed" } },
         None => rsx! { div { "Loading dogs..." } },
     })
+    // ANCHOR_END: render
+}
+
+#[inline_props]
+fn RandomDog(cx: Scope, breed: String) -> Element {
+    // ANCHOR: dependency
+    let future = use_future(&cx, (breed,), |(breed)| async move {
+        reqwest::get(format!("https://dog.ceo/api/breed/{breed}/images/random"))
+            .await
+            .unwrap()
+            .json::<ApiResponse>()
+            .await
+    });
+    // ANCHOR_END: dependency
+
+    None
 }
