b96906c770d6e40a61621f149f0e79432f2b4590
Ar37-rs <adyaro37@gmail.com>
2022-07-06 19:21:36 +0700

    Update(dep): tauri 0.19.x

--- packages/desktop/src/events.rs
+++ packages/desktop/src/events.rs
@@ -6,8 +6,9 @@ use std::sync::Arc;
 use dioxus_core::{ElementId, EventPriority, UserEvent};
 use dioxus_html::event_bubbles;
 use dioxus_html::on::*;
+use serde::{Serialize,Deserialize};
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub(crate) struct IpcMessage {
     method: String,
     params: serde_json::Value,
@@ -33,7 +34,7 @@ pub(crate) fn parse_ipc_message(payload: &str) -> Option<IpcMessage> {
     }
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct ImEvent {
     event: String,
     mounted_dom_id: u64,
