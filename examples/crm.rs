e578923a491423ac9e7f97c16574c837479f45bd
Adrian Wannenmacher <tfld@tfld.dev>
2022-08-09 22:52:51 +0200

    update CRM example

--- examples/crm.rs
+++ examples/crm.rs
@@ -2,10 +2,10 @@
 Tiny CRM: A port of the Yew CRM example to Dioxus.
 */
 use dioxus::prelude::*;
-use dioxus_router::{Link, Route, Router};
+use dioxus_router::prelude::*;
 
 fn main() {
-    dioxus_desktop::launch(app);
+    dioxus_desktop::launch(App);
 }
 
 #[derive(Clone, Debug, Default)]
@@ -15,92 +15,178 @@ pub struct Client {
     pub description: String,
 }
 
-fn app(cx: Scope) -> Element {
-    let clients = use_ref(&cx, || vec![] as Vec<Client>);
-    let firstname = use_state(&cx, String::new);
-    let lastname = use_state(&cx, String::new);
+#[allow(non_snake_case)]
+fn App(cx: Scope) -> Element {
+    let routes = use_segment(&cx, || {
+        Segment::new()
+            .index(ClientList as Component)
+            .fixed("new", Route::new(ClientAdd as Component).name(ClientAdd))
+            .fixed("settings", Route::new(Settings as Component).name(Settings))
+    });
+
+    use_context_provider::<Vec<Client>>(&cx, Vec::new);
+
+    cx.render(rsx! {
+        link {
+            rel: "stylesheet",
+            href: "https://unpkg.com/purecss@2.0.6/build/pure-min.css",
+            integrity: "sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5",
+            crossorigin: "anonymous",
+        }
+
+        style { "
+            .red {{
+                background-color: rgb(202, 60, 60) !important;
+            }}
+        " }
+
+        h1 {"Dioxus CRM Example"}
+
+        Router {
+            routes: routes.clone(),
+            Outlet { }
+        }
+    })
+}
+
+#[allow(non_snake_case)]
+fn ClientList(cx: Scope) -> Element {
+    let clients = use_context::<Vec<Client>>(&cx).unwrap();
+
+    cx.render(rsx! {
+        h2 { "List of Clients" }
+
+        Link {
+            target: (ClientAdd, vec![], None),
+            class: "pure-button pure-button-primary",
+            "Add Client"
+        }
+        Link {
+            target: (Settings, vec![], None),
+            class: "pure-button",
+            "Settings"
+        }
+
+        clients.read().iter().map(|client| rsx! {
+            div {
+                class: "client",
+                style: "margin-bottom: 50px",
+
+                p { "Name: {client.first_name} {client.last_name}" }
+                p { "Description: {client.description}" }
+            }
+        })
+    })
+}
+
+#[allow(non_snake_case)]
+fn ClientAdd(cx: Scope) -> Element {
+    let clients = use_context::<Vec<Client>>(&cx).unwrap();
+    let first_name = use_state(&cx, String::new);
+    let last_name = use_state(&cx, String::new);
     let description = use_state(&cx, String::new);
 
-    cx.render(rsx!(
-        body {
-            margin_left: "35%",
-            link {
-                rel: "stylesheet",
-                href: "https://unpkg.com/purecss@2.0.6/build/pure-min.css",
-                integrity: "sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5",
-                crossorigin: "anonymous",
-            }
-            h1 {"Dioxus CRM Example"}
-            Router {
-                Route { to: "/",
-                    div { class: "crm",
-                        h2 { margin_bottom: "10px", "List of clients" }
-                        div { class: "clients", margin_left: "10px",
-                            clients.read().iter().map(|client| rsx!(
-                                div { class: "client", style: "margin-bottom: 50px",
-                                    p { "First Name: {client.first_name}" }
-                                    p { "Last Name: {client.last_name}" }
-                                    p {"Description: {client.description}"}
-                                })
-                            )
-                        }
-                        Link { to: "/new", class: "pure-button pure-button-primary", "Add New" }
-                        Link { to: "/new", class: "pure-button", "Settings" }
+    let navigator = use_navigate(&cx).unwrap();
+
+    cx.render(rsx! {
+        h2 { "Add new Client" }
+
+        form {
+            class: "pure-form pure-form-aligned",
+            onsubmit: move |_| {
+                clients.write().push(Client {
+                    first_name: first_name.to_string(),
+                    last_name: last_name.to_string(),
+                    description: description.to_string(),
+                });
+
+                navigator.push((RootIndex, vec![], None));
+            },
+
+            fieldset {
+                div {
+                    class: "pure-control-group",
+                    label {
+                        "for": "first_name",
+                        "First Name"
+                    }
+                    input {
+                        id: "first_name",
+                        "type": "text",
+                        placeholder: "First Name…",
+                        required: "",
+                        value: "{first_name}",
+                        oninput: move |e| first_name.set(e.value.clone())
                     }
                 }
-                Route { to: "/new",
-                    div { class: "crm",
-                        h2 { margin_bottom: "10px", "Add new client" }
-                        form { class: "pure-form",
-                            input {
-                                class: "new-client firstname",
-                                placeholder: "First name",
-                                value: "{firstname}",
-                                oninput: move |e| firstname.set(e.value.clone())
-                            }
-                            input {
-                                class: "new-client lastname",
-                                placeholder: "Last name",
-                                value: "{lastname}",
-                                oninput: move |e| lastname.set(e.value.clone())
-                            }
-                            textarea {
-                                class: "new-client description",
-                                placeholder: "Description",
-                                value: "{description}",
-                                oninput: move |e| description.set(e.value.clone())
-                            }
-                        }
-                        button {
-                            class: "pure-button pure-button-primary",
-                            onclick: move |_| {
-                                clients.write().push(Client {
-                                    description: description.to_string(),
-                                    first_name: firstname.to_string(),
-                                    last_name: lastname.to_string(),
-                                });
-                                description.set(String::new());
-                                firstname.set(String::new());
-                                lastname.set(String::new());
-                            },
-                            "Add New"
-                        }
-                        Link { to: "/", class: "pure-button", "Go Back" }
+
+                div {
+                    class: "pure-control-group",
+                    label {
+                        "for": "last_name",
+                        "Last Name"
+                    }
+                    input {
+                        id: "last_name",
+                        "type": "text",
+                        placeholder: "Last Name…",
+                        required: "",
+                        value: "{last_name}",
+                        oninput: move |e| last_name.set(e.value.clone())
                     }
                 }
-                Route { to: "/settings",
-                    div {
-                        h2 { margin_bottom: "10px", "Settings" }
-                        button {
-                            background: "rgb(202, 60, 60)",
-                            class: "pure-button pure-button-primary",
-                            onclick: move |_| clients.write().clear(),
-                            "Remove all clients"
-                        }
-                        Link { to: "/", class: "pure-button pure-button-primary", "Go Back" }
+
+                div {
+                    class: "pure-control-group",
+                    label {
+                        "for": "description",
+                        "Description"
+                    }
+                    textarea {
+                        id: "description",
+                        placeholder: "Description…",
+                        value: "{description}",
+                        oninput: move |e| description.set(e.value.clone())
+                    }
+                }
+
+                div {
+                    class: "pure-controls",
+                    button {
+                        "type": "submit",
+                        class: "pure-button pure-button-primary",
+                        "Save"
+                    }
+                    Link {
+                        target: (RootIndex, vec![], None),
+                        class: "pure-button pure-button-primary red",
+                        "Cancel"
                     }
                 }
             }
+
+
         }
-    ))
+    })
+}
+
+#[allow(non_snake_case)]
+fn Settings(cx: Scope) -> Element {
+    let clients = use_context::<Vec<Client>>(&cx).unwrap();
+
+    cx.render(rsx! {
+        h2 { "Settings" }
+
+        button {
+            class: "pure-button pure-button-primary red",
+            onclick: move |_| clients.write().clear(),
+            "Remove all Clients"
+        }
+
+        Link {
+            target: (RootIndex, vec![], None),
+            class: "pure-button",
+            "Go back"
+        }
+    })
 }
