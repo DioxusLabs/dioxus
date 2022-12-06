998f9b9ec9a943b244d0a624b8648d6cee4b4f57
Adrian Wannenmacher <tfld@tfld.dev>
2022-12-06 10:11:22 +0100

    clean out router

--- packages/router/src/lib.rs
+++ packages/router/src/lib.rs
@@ -1,32 +1,3 @@
-#![doc = include_str!("../README.md")]
-#![warn(missing_docs)]
-
-mod hooks {
-    mod use_route;
-    mod use_router;
-    pub use use_route::*;
-    pub use use_router::*;
+pub mod prelude {
+    pub use dioxus_router_core::prelude::*;
 }
-pub use hooks::*;
-
-mod components {
-    #![allow(non_snake_case)]
-
-    mod link;
-    mod redirect;
-    mod route;
-    mod router;
-
-    pub use link::*;
-    pub use redirect::*;
-    pub use route::*;
-    pub use router::*;
-}
-pub use components::*;
-
-mod cfg;
-mod routecontext;
-mod service;
-
-pub use routecontext::*;
-pub use service::*;
