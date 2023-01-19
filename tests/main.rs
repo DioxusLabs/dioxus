eb91d779cf433bb7ff32ea490ff9d57992535443
mgbvox <mgbvox@gmail.com>
2023-01-19 10:18:36 -0500

    started tests; some minor build optimization

--- tests/main.rs
+++ tests/main.rs
@@ -1,4 +1,20 @@
+use std::error::Error;
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+
 #[test]
 fn ready() {
     println!("Compiled successfully!")
+}
+
+#[test]
+fn test_create() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("dioxus")?;
+    cmd.arg("create").arg("scratch");
+    cmd.assert()
+        .failure();
+
+    Ok(())
 }
