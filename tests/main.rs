e38092fc4451be54162ec73af5b912522819cbcb
mgbvox <mgbvox@gmail.com>
2023-01-19 11:35:25 -0500

    fixed cargo create bug

--- tests/main.rs
+++ tests/main.rs
@@ -8,13 +8,3 @@ use std::process::Command; // Run programs
 fn ready() {
     println!("Compiled successfully!")
 }
-
-#[test]
-fn test_create() -> Result<(), Box<dyn Error>> {
-    let mut cmd = Command::cargo_bin("dioxus")?;
-    cmd.arg("create").arg("scratch");
-    cmd.assert()
-        .failure();
-
-    Ok(())
-}
