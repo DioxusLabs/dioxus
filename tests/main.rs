7e7ff2cb4c5d7b4489c6e2db168b847539cbc11a
mgbvox <mgbvox@gmail.com>
2023-01-19 11:36:03 -0500

    removed dead code

--- tests/main.rs
+++ tests/main.rs
@@ -1,9 +1,3 @@
-use std::error::Error;
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
-
-
 #[test]
 fn ready() {
     println!("Compiled successfully!")
