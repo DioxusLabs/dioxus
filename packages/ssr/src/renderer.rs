4bf655851b38991a74c322d6c5805a892e8ef435
Evan Almloff <ealmlof1@stumail.jccc.edu>
2023-02-14 09:56:39 -0600

    fix pre tag typo

--- packages/ssr/src/renderer.rs
+++ packages/ssr/src/renderer.rs
@@ -119,7 +119,7 @@ impl Renderer {
 
                     DynamicNode::Placeholder(_el) => {
                         if self.pre_render {
-                            write!(buf, "<pre><pre/>")?;
+                            write!(buf, "<pre></pre>")?;
                         }
                     }
                 },
