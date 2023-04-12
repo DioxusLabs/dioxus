c2b873c650f68c8925cd01608af039de1c930cb0
Evan Almloff <ealmlof1@stumail.jccc.edu>
2023-04-12 11:39:57 -0500

    restore core changes

--- packages/core/src/scopes.rs
+++ packages/core/src/scopes.rs
@@ -591,8 +591,6 @@ impl<'src> ScopeState {
                         propagates: event.propagates,
                         data,
                     });
-                } else {
-                    println!("Failed to downcast event data")
                 }
             }))
         };
