a78ee4ffe77ad68a2b6e677446853bef3b086584
stephenandary <stephen@jamminmusic.com>
2023-05-01 14:12:08 -0400

    WIP - Update devcontainer to use debian bookworm

--- .devcontainer/local-features/linux/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Update Apt Database"
-sudo apt-get update
-
-echo "Install Webkit Dependencies"
-sudo apt-get -qq install build-essential libwebkit2gtk-4.0-dev libgtk-3-dev libayatana-appindicator3-dev
