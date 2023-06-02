808da6e07fb03515c4268a44f8c7c519a813bbfd
Timo Kösters <timo@koesters.xyz>
2023-06-02 07:31:40 +0200

    fix: disable base_path changes for serve

--- src/cli/serve/mod.rs
+++ src/cli/serve/mod.rs
@@ -36,6 +36,9 @@ impl Serve {
             crate_config.set_features(self.serve.features.unwrap());
         }
 
+        // Subdirectories don't work with the server
+        crate_config.dioxus_config.web.app.base_path = None;
+
         let platform = self.serve.platform.unwrap_or_else(|| {
             crate_config
                 .dioxus_config
