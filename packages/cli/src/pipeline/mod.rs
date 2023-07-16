f32b324c5f8f29361c0f50bd191a7527972d08b6
Miles Murgaw <milesmurgaw@gmail.com>
2023-07-16 17:36:11 -0400

    progress: pipeline

--- packages/cli/src/pipeline/mod.rs
+++ packages/cli/src/pipeline/mod.rs
@@ -3,19 +3,26 @@ use std::path::PathBuf;
 
 pub mod wasm;
 
+/// Represents a file's type.
 pub enum FileType {
     JavaScript,
     Css,
     // SASS & SCSS
     SassType,
+    Wasm,
 }
 
+/// Represents a File on the device's storage system.
 pub struct File {
-    pub name: String,
-    pub path: PathBuf,
-    pub file_type: FileType,
+    /// The name of the file.
+    name: String,
+    /// The path to the file.
+    path: PathBuf,
+    /// The file's type.
+    file_type: FileType,
 }
 
+/// Represents a pipeline with it's own config and steps.
 pub struct Pipeline {
     config: PipelineConfig,
     steps: Vec<Box<dyn PipelineStep>>,
@@ -25,7 +32,7 @@ impl Pipeline {
     /// Build a new pipeline.
     pub fn new(config: PipelineConfig) -> Self {
         // Collect all input files
-        
+
         // Create config struct
 
         // Return self
@@ -51,28 +58,73 @@ impl Pipeline {
     }
 }
 
+/// Configures the pipeline with the information it needs to complete.
 pub struct PipelineConfig {
-    crate_path: PathBuf,
-    release: bool,
-    verbose: bool,
-    features: Vec<String>,
+    /// Information related to the pipeline's target crate.
+    crate_info: CrateInfo,
+    /// Information related to how the pipeline should build the target crate.
+    build_config: BuildConfig,
+    /// Represents the raw source files.
     input_files: Vec<File>,
+    /// Represents either a completed or processed artifact from the pipeline.
     output_files: Vec<File>,
 }
 
 impl PipelineConfig {
-    pub fn new(crate_path: PathBuf, release: bool, verbose: bool, features: Vec<String>) -> Self {
+    /// Create a new PipelineConfig
+    pub fn new(crate_info: CrateInfo, build_config: BuildConfig) -> Self {
         Self {
-            crate_path,
-            release,
-            verbose,
-            features,
+            crate_info,
+            build_config,
             input_files: Vec::new(),
             output_files: Vec::new(),
         }
     }
 }
 
+/// Represents information about a crate.
+pub struct CrateInfo {
+    /// If the crate is in a workspace this value will point to the workspace root.
+    workspace_path: Option<PathBuf>,
+    /// The path to the crate itself.
+    path: PathBuf,
+    /// The name of the crate.
+    name: String,
+}
+
+impl CrateInfo {
+    /// Creates a new CrateInfo
+    pub fn new(workspace_path: Option<PathBuf>, path: PathBuf, name: String) -> Self {
+        Self {
+            workspace_path,
+            path,
+            name,
+        }
+    }
+}
+
+/// Describes how the pipeline should build the target crate.
+pub struct BuildConfig {
+    /// Whether the pipeline should run for release mode.
+    release: bool,
+    /// Whether the pipeline should emit more verbose information.
+    verbose: bool,
+    /// The features the pipeline should build for.
+    features: Vec<String>,
+}
+
+impl BuildConfig {
+    /// Create a new BuildConfig
+    pub fn new(release: bool, verbose: bool, features: Vec<String>) -> Self {
+        Self {
+            release,
+            verbose,
+            features,
+        }
+    }
+}
+
+/// Represents a step in the pipeline.
 pub trait PipelineStep {
     fn run(&mut self, config: &mut PipelineConfig) -> Result<()>;
 }
--- packages/cli/src/pipeline/wasm.rs
+++ packages/cli/src/pipeline/wasm.rs
@@ -1,8 +1,9 @@
-use std::process::Command;
-
-use crate::Error;
-
 use super::{PipelineConfig, PipelineStep};
+use crate::pipeline::{File, FileType};
+use std::path::PathBuf;
+
+const DEBUG_TARGET: &str = "/target/debug";
+const RELEASE_TARGET: &str = "/target/release";
 
 pub struct WasmBuild {}
 
@@ -14,21 +15,55 @@ impl PipelineStep for WasmBuild {
         let mut cmd = subprocess::Exec::cmd("cargo");
 
         cmd = cmd
-            .cwd(config.crate_path.clone())
+            .cwd(config.crate_info.path.clone())
             .arg("build")
             .arg("--target")
             .arg("wasm32-unknown-unknown");
 
-        if config.release {
+        if config.build_config.release {
             cmd = cmd.arg("--release");
         }
 
-        if config.verbose {
+        if config.build_config.verbose {
             cmd = cmd.arg("--verbose");
         }
 
+        if !config.build_config.features.is_empty() {
+            cmd = cmd.arg("--features");
+            cmd = cmd.arg(config.build_config.features.join(" "))
+        }
+
         // Run command
-        //cmd.detached().map_err(|e| Error::BuildFailed(e.to_string()))?;
+
+        // Get the target 'inner' path
+        // /target/{X}
+        let target_path = if config.build_config.release {
+            PathBuf::from(RELEASE_TARGET)
+        } else {
+            PathBuf::from(DEBUG_TARGET)
+        };
+
+        // Get the full path to target
+        // {PATH}/target/{X}
+        let target_path = if let Some(workspace_path) = &config.crate_info.workspace_path {
+            workspace_path.join(target_path)
+        } else {
+            config.crate_info.path.join(target_path)
+        };
+
+        // Get the final path to the built wasm file
+        // {PATH}/target/{X}/{CRATE_NAME}.wasm
+        let wasm_out_path = target_path.join(format!("{}.wasm", config.crate_info.name));
+
+        // Create the file metadata
+        let out_file = File {
+            name: config.crate_info.name.clone(),
+            path: wasm_out_path,
+            file_type: FileType::Wasm,
+        };
+
+        // Push it to out files for later processing
+        config.output_files.push(out_file);
 
         log::info!("Finished building wasm.");
         Ok(())
