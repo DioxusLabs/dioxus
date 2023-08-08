aaded7981fbad2a9e11438fb3d61ec56c80cebc6
Evan Almloff <evanalmloff@gmail.com>
2023-08-08 10:56:41 -0700

    fix boolean attribute rendering in SSR

---
+++ packages/ssr/tests/bool_attr.rs
@@ -0,0 +1,38 @@
+use dioxus::prelude::*;
+
+#[test]
+fn static_boolean_attributs() {
+    fn app(cx: Scope) -> Element {
+        render! {
+            div { hidden: "false" }
+            div { hidden: "true" }
+        }
+    }
+
+    let mut dom = VirtualDom::new(app);
+    _ = dom.rebuild();
+
+    assert_eq!(
+        dioxus_ssr::render(&dom),
+        r#"<div></div><div hidden="true"></div>"#
+    );
+}
+
+#[test]
+fn dynamic_boolean_attributs() {
+    fn app(cx: Scope) -> Element {
+        let inner_html = "<div>1234</div>";
+        render! {
+            div { hidden: false }
+            div { hidden: true }
+        }
+    }
+
+    let mut dom = VirtualDom::new(app);
+    _ = dom.rebuild();
+
+    assert_eq!(
+        dioxus_ssr::render(&dom),
+        r#"<div></div><div hidden=true></div>"#
+    );
+}
