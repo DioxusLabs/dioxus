124b58afb1bc6cd9b7dd9573fa837155fe3ef7a6
Evan Simkowitz <esimkowitz@wustl.edu>
2023-08-24 00:06:57 -0400

    missed one

--- packages/rink/src/widgets/text_like.rs
+++ packages/rink/src/widgets/text_like.rs
@@ -1,7 +1,9 @@
 use std::{collections::HashMap, io::stdout};
 
 use crossterm::{cursor::MoveTo, execute};
-use dioxus_html::{input_data::keyboard_types::Key, KeyboardData, MouseData, prelude::PointInteraction};
+use dioxus_html::{
+    input_data::keyboard_types::Key, prelude::PointInteraction, KeyboardData, MouseData,
+};
 use dioxus_native_core::{
     custom_element::CustomElement,
     node::OwnedAttributeDiscription,
