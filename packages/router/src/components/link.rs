8bcd0b378f6885674770cbfac82669116ebc2d8e
Leonard <tigerros.gh@gmail.com>
2023-09-11 19:50:39 +0200

    Allow the type complexity lint for the entire link.rs file

--- packages/router/src/components/link.rs
+++ packages/router/src/components/link.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::type_complexity)]
+
 use std::any::Any;
 use std::fmt::Debug;
 use std::rc::Rc;
@@ -52,7 +54,6 @@ impl From<&str> for IntoRoutable {
 }
 
 /// The properties for a [`Link`].
-#[allow(clippy::type_complexity)]
 #[derive(Props)]
 pub struct LinkProps<'a> {
     /// A class to apply to the generate HTML anchor tag if the `target` route is active.
