dca58b23e0c30e436fff88b52371af5432b9c947
Leonard <tigerros.gh@gmail.com>
2023-10-10 03:26:58 +0200

    Include README in docs.rs and add Dioxus logo

--- packages/dioxus-tui/src/lib.rs
+++ packages/dioxus-tui/src/lib.rs
@@ -1,3 +1,7 @@
+#![doc = include_str!("../README.md")]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/79236386")]
+#![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/79236386")]
+
 mod element;
 
 use std::{
--- packages/server-macro/src/lib.rs
+++ packages/server-macro/src/lib.rs
@@ -1,3 +1,7 @@
+// TODO: Create README, uncomment this: #![doc = include_str!("../README.md")]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/79236386")]
+#![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/79236386")]
+
 use convert_case::{Case, Converter};
 use proc_macro::TokenStream;
 use proc_macro2::Literal;
