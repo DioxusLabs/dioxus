2d1234c3d11d0950f4f12272cac9592a00723f1e
Leonard <tigerros.gh@gmail.com>
2023-10-10 17:52:48 +0200

    Include README and Dioxus logo in package docs (#1536)
    * Include README in docs.rs and add Dioxus logo
    
    * Update README.md

--- packages/hooks/src/lib.rs
+++ packages/hooks/src/lib.rs
@@ -1,3 +1,6 @@
+#![doc = include_str!("../README.md")]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/79236386")]
+#![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/79236386")]
 #![cfg_attr(feature = "nightly-features", feature(debug_refcell))]
 
 #[macro_export]
