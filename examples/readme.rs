83f7ef9a447547c720e310b10e1682dc7762bdbc
Evan Almloff <evanalmloff@gmail.com>
2023-11-19 09:58:00 -0600

    add an optional cleanup closure to the use effect hook

--- examples/readme.rs
+++ examples/readme.rs
@@ -11,6 +11,12 @@ fn main() {
 fn app(cx: Scope) -> Element {
     let mut count = use_state(cx, || 0);
 
+    use_effect(cx, (), move |()| async {});
+
+    use_effect(cx, (count.get(),), move |(count,)| async move {
+        move || println!("Count unmounted from {}", count)
+    });
+
     cx.render(rsx! {
         h1 { "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
