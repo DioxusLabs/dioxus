c83ca7b15f8a28c85a83957da875a0ac2c04bb22
Evan Almloff <evanalmloff@gmail.com>
2024-01-23 10:09:14 -0600

    fix raw expressions in the format macro

--- packages/rsx/src/ifmt.rs
+++ packages/rsx/src/ifmt.rs
@@ -86,10 +86,10 @@ impl IfmtInput {
                         match single_dynamic {
                             Some(current_string) => {
                                 single_dynamic =
-                                    Some(quote!(#current_string + &#segment.to_string()));
+                                    Some(quote!(#current_string + &(#segment).to_string()));
                             }
                             None => {
-                                single_dynamic = Some(quote!(#segment.to_string()));
+                                single_dynamic = Some(quote!((#segment).to_string()));
                             }
                         }
                     } else {
