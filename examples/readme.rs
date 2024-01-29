1c67585ba25499a6582aed98a835cb14e4298f7d
Evan Almloff <evanalmloff@gmail.com>
2024-01-29 13:37:00 -0600

    restore readme.rs

--- examples/readme.rs
+++ examples/readme.rs
@@ -8,20 +8,7 @@ fn app() -> Element {
     let mut count = use_signal(|| 0);
 
     rsx! {
-        Child { count: "High-Five counter: {count}" }
-        Child { count: "count" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
-    }
-}
-
-#[derive(Props, Clone, PartialEq)]
-struct ChildProps {
-    count: Option<String>,
-}
-
-fn Child(props: ChildProps) -> Element {
-    rsx! {
-        h1 { "{props.count.unwrap_or_default()}" }
     }
 }
