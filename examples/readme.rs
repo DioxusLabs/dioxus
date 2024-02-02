8b489db5ac36013b42eaff0f96cda63d306d81e0
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-02-02 15:03:46 -0800

    fix readme

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use dioxus::prelude::*;
 
 fn main() {
@@ -8,11 +6,6 @@ fn main() {
 
 fn app() -> Element {
     let mut count = use_signal(|| 0);
-
-    let g: ReadOnlySignal<(), UnsyncStorage> =
-        use_maybe_sync_selector_with_dependencies((&(count())), move |deps| {
-            println!("High-Five counter: {}", deps);
-        });
 
     rsx! {
         h1 { "High-Five counter: {count}" }
