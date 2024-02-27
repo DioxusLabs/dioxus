271b53e86662efbf9357c780a1140ac34d9b1628
Evan Almloff <evanalmloff@gmail.com>
2024-02-27 11:46:40 -0600

    Update the liveview renderer to the new sledgehammer version

--- packages/liveview/src/main.js
+++ packages/liveview/src/main.js
@@ -9,6 +9,7 @@ function main() {
 
 class IPC {
   constructor(root) {
+    window.interpreter = new JSChannel();
     window.interpreter.initialize(root);
     const ws = new WebSocket(WS_ADDR);
     ws.binaryType = "arraybuffer";
@@ -34,7 +35,7 @@ class IPC {
       // The first byte tells the shim if this is a binary of text frame
       if (binaryFrame) {
         // binary frame
-        run_from_bytes(messageData);
+        window.interpreter.run_from_bytes(messageData);
       }
       else {
         // text frame
