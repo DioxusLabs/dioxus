a513ba9b0146c2172d7e079590fb7897cebb2705
marc2332 <mespinsanz@gmail.com>
2024-03-31 16:49:33 +0200

    fmt

--- packages/hooks/src/use_on_destroy.rs
+++ packages/hooks/src/use_on_destroy.rs
@@ -1,8 +1,6 @@
 use dioxus_core::prelude::use_drop;
 
-#[deprecated(
-    note = "Use `use_drop` instead, which has the same functionality."
-)]
+#[deprecated(note = "Use `use_drop` instead, which has the same functionality.")]
 pub fn use_on_unmount<D: FnOnce() + 'static>(destroy: D) {
     use_drop(destroy);
 }
