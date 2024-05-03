396f210a240ab5b05faa52ae5f112308c79a2a89
Evan Almloff <evanalmloff@gmail.com>
2024-05-03 13:33:24 -0500

    fix clippy empty docs

--- packages/interpreter/src/lib.rs
+++ packages/interpreter/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::empty_docs)]
 #![doc = include_str!("../README.md")]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/79236386")]
 #![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/79236386")]
