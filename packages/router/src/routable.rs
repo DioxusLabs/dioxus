9783962ddb6c7f43f398b25bd3ce869866217df6
Evan Almloff <evanalmloff@gmail.com>
2024-05-31 17:54:01 -0500

    add examples to each of the router traits

--- packages/router/src/routable.rs
+++ packages/router/src/routable.rs
@@ -24,11 +24,48 @@ impl<E: Display> Display for RouteParseError<E> {
     }
 }
 
-/// Something that can be created from an entire query string.
+/// Something that can be created from an entire query string. This trait must be implemented for any type that is spread into the query segment like `#[route("/?:..query")]`.
 ///
-/// This trait needs to be implemented if you want to turn a query string into a struct.
 ///
-/// A working example can be found in the `examples` folder in the root package under `query_segments_demo`.
+/// **This trait is automatically implemented for any types that implement `From<&str>`.**
+///
+/// ```rust
+/// use dioxus::prelude::*;
+///
+/// #[derive(Routable, Clone, PartialEq, Debug)]
+/// enum Route {
+///     // FromQuery must be implemented for any types you spread into the query segment
+///     #[route("/?:..query")]
+///     Home {
+///         query: CustomQuery
+///     },
+/// }
+///
+/// struct CustomQuery {
+///     count: i32,
+/// }
+///
+/// // We implement From<&str> for CustomQuery so that FromQuery is implemented automatically
+/// impl From<&str> for CustomQuery {
+///     fn from(query: &str) -> Self {
+///         CustomQuery {
+///             count: query.parse().unwrap_or(0),
+///         }
+///     }
+/// }
+///
+/// // We also need to implement Display for CustomQuery which will be used to format the query string into the URL
+/// impl Display for CustomQuery {
+///     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+///         write!(f, "{}", self.count)
+///     }
+/// }
+///
+/// # #[component]
+/// # fn Home(query: CustomQuery) -> Element {
+/// #     todo!()
+/// # }
+/// ```
 pub trait FromQuery {
     /// Create an instance of `Self` from a query string.
     fn from_query(query: &str) -> Self;
@@ -40,9 +77,55 @@ impl<T: for<'a> From<&'a str>> FromQuery for T {
     }
 }
 
-/// Something that can be created from a query argument.
+/// Something that can be created from a query argument. This trait must be implemented for any type that is used as a query argument like `#[route("/?:query")]`.
 ///
-/// This trait must be implemented for every type used within a query string in the router macro.
+/// **This trait is automatically implemented for any types that implement `FromStr` and `Default`.**
+///
+/// ```rust
+/// use dioxus::prelude::*;
+///
+/// #[derive(Routable, Clone, PartialEq, Debug)]
+/// enum Route {
+///     // FromQuerySegment must be implemented for any types you use in the query segment
+///     // When you don't spread the query, you can parse multiple values form the query
+///     // This url will be in the format `/?query=123&other=456`
+///     #[route("/?:query&:other")]
+///     Home {
+///         query: CustomQuery,
+///         other: i32,
+///     },
+/// }
+///
+/// // We can derive Default for CustomQuery
+/// // If the router fails to parse the query value, it will use the default value instead
+/// #[derive(Default)]
+/// struct CustomQuery {
+///     count: i32,
+/// }
+///
+/// // We implement FromStr for CustomQuery so that FromQuerySegment is implemented automatically
+/// impl FromStr for CustomQuery {
+///     type Err = <i32 as FromStr>::Err;
+///
+///     fn from_str(query: &str) -> Result<Self, Self::Err> {
+///         Ok(CustomQuery {
+///             count: query.parse()?,
+///         })
+///     }
+/// }
+///
+/// // We also need to implement Display for CustomQuery which will be used to format the query string into the URL
+/// impl Display for CustomQuery {
+///     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+///         write!(f, "{}", self.count)
+///     }
+/// }
+///
+/// # #[component]
+/// # fn Home(query: CustomQuery, other: i32) -> Element {
+/// #     todo!()
+/// # }
+/// ```
 pub trait FromQueryArgument: Default {
     /// The error that can occur when parsing a query argument.
     type Err;
@@ -68,9 +151,10 @@ where
     }
 }
 
-/// Something that can be created from an entire hash fragment.
+/// Something that can be created from an entire hash fragment. This must be implemented for any type that is used as a hash fragment like `#[route("/#:hash_fragment")]`.
 ///
-/// This trait needs to be implemented if you want to turn a hash fragment into a struct.
+///
+/// **This trait is automatically implemented for any types that implement `FromStr` and `Default`.**
 ///
 /// # Example
 ///
@@ -144,7 +228,56 @@ where
     }
 }
 
-/// Something that can be created from a route segment.
+/// Something that can be created from a single route segment. This must be implemented for any type that is used as a route segment like `#[route("/:route_segment")]`.
+///
+///
+/// **This trait is automatically implemented for any types that implement `FromStr` and `Default`.**
+///
+/// ```rust
+/// use dioxus::prelude::*;
+///
+/// #[derive(Routable, Clone, PartialEq, Debug)]
+/// enum Route {
+///     // FromRouteSegment must be implemented for any types you use in the route segment
+///     // When you don't spread the route, you can parse multiple values from the route
+///     // This url will be in the format `/123/456`
+///     #[route("/:route_segment_one/:route_segment_two")]
+///     Home {
+///         route_segment_one: CustomRouteSegment,
+///         route_segment_two: i32,
+///     },
+/// }
+///
+/// // We can derive Default for CustomRouteSegment
+/// // If the router fails to parse the route segment, it will use the default value instead
+/// #[derive(Default)]
+/// struct CustomRouteSegment {
+///     count: i32,
+/// }
+///
+/// // We implement FromStr for CustomRouteSegment so that FromRouteSegment is implemented automatically
+/// impl FromStr for CustomRouteSegment {
+///     type Err = <i32 as FromStr>::Err;
+///
+///     fn from_str(route_segment: &str) -> Result<Self, Self::Err> {
+///         Ok(CustomRouteSegment {
+///             count: route_segment.parse()?,
+///         })
+///     }
+/// }
+///
+/// // We also need to implement Display for CustomRouteSegment which will be used to format the route segment into the URL
+/// impl Display for CustomRouteSegment {
+///     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+///         write!(f, "{}", self.count)
+///     }
+/// }
+///
+/// # #[component]
+/// # fn Home(route_segment_one: CustomRouteSegment, route_segment_two: i32) -> Element {
+/// #     todo!()
+/// # }
+/// ```
 pub trait FromRouteSegment: Sized {
     /// The error that can occur when parsing a route segment.
     type Err;
@@ -170,7 +303,58 @@ fn full_circle() {
     assert_eq!(String::from_route_segment(route).unwrap(), route);
 }
 
-/// Something that can be converted to route segments.
+/// Something that can be converted into multiple route segments. This must be implemented for any type that is spread into the route segment like `#[route("/:..route_segments")]`.
+///
+///
+/// **This trait is automatically implemented for any types that implement `IntoIterator<Item=impl Display>`.**
+///
+/// ```rust
+/// use dioxus::prelude::*;
+///
+/// #[derive(Routable, Clone, PartialEq, Debug)]
+/// enum Route {
+///     // FromRouteSegments must be implemented for any types you use in the route segment
+///     // When you spread the route, you can parse multiple values from the route
+///     // This url will be in the format `/123/456/789`
+///     #[route("/:..numeric_route_segments")]
+///     Home {
+///         numeric_route_segments: NumericRouteSegments,
+///     },
+/// }
+///
+/// // We can derive Default for NumericRouteSegments
+/// // If the router fails to parse the route segment, it will use the default value instead
+/// #[derive(Default)]
+/// struct NumericRouteSegments {
+///     numbers: Vec<i32>,
+/// }
+///
+/// // We implement IntoIterator for NumericRouteSegments so that FromRouteSegments is implemented automatically
+/// impl IntoIterator for NumericRouteSegments {
+///     type Item = i32;
+///     type IntoIter = std::vec::IntoIter<Self::Item>;
+///
+///     fn into_iter(self) -> Self::IntoIter {
+///         self.count.into_iter()
+///     }
+/// }
+///
+/// // We also need to parse the route segments with `FromRouteSegments`
+/// impl FromRouteSegments for NumericRouteSegments {
+///     fn from_route_segments(segments: &[&str]) -> Result<Self, Self::Err> {
+///         let mut numbers = Vec::new();
+///         for segment in segments {
+///             numbers.push(segment.parse()?);
+///         }
+///         Ok(NumericRouteSegments { numbers })
+///     }
+/// }
+///
+/// # #[component]
+/// # fn Home(numeric_route_segments: NumericRouteSegments) -> Element {
+/// #     todo!()
+/// # }
+/// ```
 pub trait ToRouteSegments {
     /// Display the route segments. You must url encode the segments.
     fn display_route_segments(self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
@@ -205,7 +389,58 @@ fn to_route_segments() {
     assert_eq!(DisplaysRoute.to_string(), "/hello/world");
 }
 
-/// Something that can be created from route segments.
+/// Something that can be created from multiple route segments. This must be implemented for any type that is spread into the route segment like `#[route("/:..route_segments")]`.
+///
+///
+/// **This trait is automatically implemented for any types that implement `FromIterator<impl Display>`.**
+///
+/// ```rust
+/// use dioxus::prelude::*;
+///
+/// #[derive(Routable, Clone, PartialEq, Debug)]
+/// enum Route {
+///     // FromRouteSegments must be implemented for any types you use in the route segment
+///     // When you spread the route, you can parse multiple values from the route
+///     // This url will be in the format `/123/456/789`
+///     #[route("/:..numeric_route_segments")]
+///     Home {
+///         numeric_route_segments: NumericRouteSegments,
+///     },
+/// }
+///
+/// // We can derive Default for NumericRouteSegments
+/// // If the router fails to parse the route segment, it will use the default value instead
+/// #[derive(Default)]
+/// struct NumericRouteSegments {
+///     numbers: Vec<i32>,
+/// }
+///
+/// // We implement IntoIterator for NumericRouteSegments so that FromRouteSegments is implemented automatically
+/// impl IntoIterator for NumericRouteSegments {
+///     type Item = i32;
+///     type IntoIter = std::vec::IntoIter<Self::Item>;
+///
+///     fn into_iter(self) -> Self::IntoIter {
+///         self.count.into_iter()
+///     }
+/// }
+///
+/// // We also need to parse the route segments with `FromRouteSegments`
+/// impl FromRouteSegments for NumericRouteSegments {
+///     fn from_route_segments(segments: &[&str]) -> Result<Self, Self::Err> {
+///         let mut numbers = Vec::new();
+///         for segment in segments {
+///             numbers.push(segment.parse()?);
+///         }
+///         Ok(NumericRouteSegments { numbers })
+///     }
+/// }
+///
+/// # #[component]
+/// # fn Home(numeric_route_segments: NumericRouteSegments) -> Element {
+/// #     todo!()
+/// # }
+/// ```
 pub trait FromRouteSegments: Sized {
     /// The error that can occur when parsing route segments.
     type Err;
@@ -238,7 +473,7 @@ type SiteMapFlattened<'a> = FlatMap<
 /// 2. Converted to a route.
 /// 3. Rendered as a component.
 ///
-/// This trait can be derived using the `#[derive(Routable)]` macro.
+/// This trait should generally be derived using the `dioxus_router_macro::Routable` macro.
 pub trait Routable: FromStr + Display + Clone + 'static {
     /// The error that can occur when parsing a route.
     const SITE_MAP: &'static [SiteMapSegment];
