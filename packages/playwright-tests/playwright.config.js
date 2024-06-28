4da2ef6bf6eab49831e31b3901c301e0fb105584
Evan Almloff <evanalmloff@gmail.com>
2024-06-28 12:29:47 -0700

    longer playwright build limit

--- packages/playwright-tests/playwright.config.js
+++ packages/playwright-tests/playwright.config.js
@@ -76,7 +76,7 @@ module.exports = defineConfig({
       command:
         "cargo run --package dioxus-playwright-liveview-test --bin dioxus-playwright-liveview-test",
       port: 3030,
-      timeout: 20 * 60 * 1000,
+      timeout: 30 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
     },
@@ -85,7 +85,7 @@ module.exports = defineConfig({
       command:
         'cargo run --package dioxus-cli --release -- serve --addr "127.0.0.1" --port 9999',
       port: 9999,
-      timeout: 20 * 60 * 1000,
+      timeout: 30 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
     },
@@ -94,7 +94,7 @@ module.exports = defineConfig({
       command:
         'cargo run --package dioxus-cli --release -- serve --platform fullstack --addr "127.0.0.1" --port 3333',
       port: 3333,
-      timeout: 20 * 60 * 1000,
+      timeout: 30 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
     },
@@ -103,7 +103,7 @@ module.exports = defineConfig({
       command:
         'cargo run --package dioxus-cli --release -- serve --platform fullstack --addr "127.0.0.1" --port 4040',
       port: 4040,
-      timeout: 20 * 60 * 1000,
+      timeout: 30 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
     },
@@ -112,7 +112,7 @@ module.exports = defineConfig({
       command:
         'cargo run --package dioxus-cli --release -- serve --platform fullstack --addr "127.0.0.1" --port 5050',
       port: 5050,
-      timeout: 20 * 60 * 1000,
+      timeout: 30 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
       stdout: "pipe",
     },
