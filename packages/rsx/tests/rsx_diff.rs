7a5fb3bd290466691b0a77aa62b6ac899c173c78
Evan Almloff <evanalmloff@gmail.com>
2024-06-27 17:16:21 -0700

    fix rsx tests

--- packages/rsx/tests/rsx_diff.rs
+++ packages/rsx/tests/rsx_diff.rs
@@ -48,7 +48,7 @@ fn create_template() {
 fn diff_template() {
     #[allow(unused, non_snake_case)]
     fn Comp() -> dioxus_core::Element {
-        VNode::empty()
+        dioxus_core::VNode::empty()
     }
 
     let input = quote! {
