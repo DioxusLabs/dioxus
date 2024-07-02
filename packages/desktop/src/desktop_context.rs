c8b4ad29ab4207fb25a8cb405f7b8e4a232d61bd
Evan Almloff <evanalmloff@gmail.com>
2024-07-02 15:56:26 -0700

    restore changes in desktop

--- packages/desktop/src/desktop_context.rs
+++ packages/desktop/src/desktop_context.rs
@@ -82,8 +82,6 @@ impl std::ops::Deref for DesktopService {
 }
 
 impl DesktopService {
-    // TODO Maybe turn this into a builder pattern?
-    #[allow(clippy::too_many_arguments)]
     pub(crate) fn new(
         webview: WebView,
         window: Window,
