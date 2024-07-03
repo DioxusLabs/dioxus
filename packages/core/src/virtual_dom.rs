f5c78df9c0296547dc6accf8035a95c37834887a
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-07-03 10:25:09 +0200

    fix: remove redundant generic arg

--- packages/core/src/virtual_dom.rs
+++ packages/core/src/virtual_dom.rs
@@ -255,7 +255,7 @@ impl VirtualDom {
     /// ```
     ///
     /// Note: the VirtualDom is not progressed, you must either "run_with_deadline" or use "rebuild" to progress it.
-    pub fn new<M: 'static>(app: impl ComponentFunction<(), M>) -> Self {
+    pub fn new(app: impl ComponentFunction<()>) -> Self {
         Self::new_with_props(app, ())
     }
 
@@ -314,7 +314,7 @@ impl VirtualDom {
     }
 
     /// Create a new virtualdom and build it immediately
-    pub fn prebuilt<M: 'static>(app: impl ComponentFunction<(), M>) -> Self {
+    pub fn prebuilt(app: impl ComponentFunction<()>) -> Self {
         let mut dom = Self::new(app);
         dom.rebuild_in_place();
         dom
