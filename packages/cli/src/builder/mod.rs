2c5f323d71c15fd1eec75600e907b1b561049b03
Evan Almloff <evanalmloff@gmail.com>
2024-07-08 15:09:57 -0700

    remove old process code

--- packages/cli/src/builder/mod.rs
+++ packages/cli/src/builder/mod.rs
@@ -16,62 +16,6 @@ mod prepare_html;
 mod progress;
 mod web;
 
-// Desktop: native build -> native process
-// Web: web build -> web process
-// Fullstack web and native build -> native process
-
-// struct LiveApplication {
-//     /// The platform specific process that is running the application
-//     process: Process,
-//     /// The websocket message channel that controls the application
-//     messages: tokio::sync::mpsc::UnboundedSender<Message>,
-// }
-
-// struct WebProcess {
-//     /// A websocket connection to any running instance of the application
-//     connections: Vec<Connection>,
-//     /// The server that is serving the application
-//     server: Server,
-// }
-
-// struct NativeProcess {
-//     child: Child,
-// }
-
-// impl NativeProcess {
-//     async fn kill(&mut self) -> Result<()> {
-//         Ok(self.child.kill().await?)
-//     }
-// }
-
-// enum Process {
-//     /// Running web applications
-//     Web(WebProcess),
-//     /// The child process that is building the application
-//     Native(NativeProcess),
-// }
-
-// impl Process {
-//     async fn launch(build_result: BuildResult) -> Result<Self> {
-//         todo!()
-//     }
-
-//     async fn send_message(&self, message: Message) -> Result<()> {
-//         todo!()
-//     }
-
-//     async fn shutdown(&mut self) -> Result<()> {
-//         // Try to cleanly shutdown the process first
-//         self.send_message(Message::Shutdown).await?;
-
-//         // Then kill the process
-//         match self {
-//             Process::Web(web) => todo!(),
-//             Process::Native(native) => native.kill().await,
-//         }
-//     }
-// }
-
 pub struct BuildRequest {
     /// Whether the build is for serving the application
     pub serve: bool,
