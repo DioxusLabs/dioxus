8855508e42c14c15755dd1b83783aabc4a41520f
Evan Almloff <evanalmloff@gmail.com>
2024-07-17 13:28:40 -0700

    make race_condition_regression test panic if it fails

--- packages/generational-box/tests/sync.rs
+++ packages/generational-box/tests/sync.rs
@@ -6,19 +6,21 @@ use generational_box::{AnyStorage, GenerationalBox, SyncStorage};
 
 #[test]
 fn race_condition_regression() {
-    let handle = {
-        let owner = SyncStorage::owner();
-        let key = owner.insert(1u64);
-        let handle = std::thread::spawn(move || reader(key));
+    for _ in 0..100 {
+        let handle = {
+            let owner = SyncStorage::owner();
+            let key = owner.insert(1u64);
+            let handle = std::thread::spawn(move || reader(key));
 
-        std::thread::sleep(Duration::from_secs(1));
-        handle
-        // owner is dropped now
-    };
-    // owner is *recycled*
-    let owner = SyncStorage::owner();
-    let _key = owner.insert(2u64);
-    let _ = handle.join();
+            std::thread::sleep(Duration::from_millis(10));
+            handle
+            // owner is dropped now
+        };
+        // owner is *recycled*
+        let owner = SyncStorage::owner();
+        let _key = owner.insert(2u64);
+        let _ = handle.join();
+    }
 }
 
 fn reader(key: GenerationalBox<u64, SyncStorage>) {
@@ -26,7 +28,7 @@ fn reader(key: GenerationalBox<u64, SyncStorage>) {
         match key.try_read() {
             Ok(value) => {
                 if *value == 2 {
-                    println!("CAUGHT A BAD ONE");
+                    panic!("Read a new value with the old generation");
                 } else {
                     // fine
                 }
