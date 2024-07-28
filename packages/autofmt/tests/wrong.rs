e73484795abbbf660ac16b7c771e8965d60bf759
opensource-inemar-net <65956766+opensource-inemar-net@users.noreply.github.com>
2024-07-28 21:06:09 +0200

    This fixes issue (#2724)

--- packages/autofmt/tests/wrong.rs
+++ packages/autofmt/tests/wrong.rs
@@ -30,3 +30,4 @@ twoway!("multiexpr-many" => multiexpr_many (IndentOptions::new(IndentType::Space
 twoway!("simple-combo-expr" => simple_combo_expr (IndentOptions::new(IndentType::Spaces, 4, false)));
 twoway!("oneline-expand" => online_expand (IndentOptions::new(IndentType::Spaces, 4, false)));
 twoway!("shortened" => shortened (IndentOptions::new(IndentType::Spaces, 4, false)));
+twoway!("syntax_error" => syntax_error (IndentOptions::new(IndentType::Spaces, 4, false)));
