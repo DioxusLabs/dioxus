acda5faefdfdfcfad7fd55046c1bfc56cfcac291
i123iu <ada.kundrat@gmail.com>
2024-07-29 15:17:54 +0200

    cargo fmt

--- packages/desktop/src/app.rs
+++ packages/desktop/src/app.rs
@@ -177,7 +177,7 @@ impl App {
             LastWindowExitsApp => {
                 #[cfg(debug_assertions)]
                 self.persist_window_state();
-                
+
                 self.webviews.remove(&id);
                 if self.webviews.is_empty() {
                     self.control_flow = ControlFlow::Exit
