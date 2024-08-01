88dffeb79038b1ddd3ca4b65b4e5e26eaedfea68
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-07-31 18:29:20 -0700

    fix: doc_cfg in static gen

--- packages/static-generation/src/lib.rs
+++ packages/static-generation/src/lib.rs
@@ -1,4 +1,9 @@
+#![doc = include_str!("readme.md")]
 #![allow(non_snake_case)]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/79236386")]
+#![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/79236386")]
+#![deny(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 mod config;
 pub use config::*;
