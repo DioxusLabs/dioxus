01d4368d0bb6509bda1f2ef8d6ad5d8554a556fe
Evan Almloff <evanalmloff@gmail.com>
2024-08-14 13:01:16 -0700

    ignore signal write warnings in hot reloading

--- packages/hot-reload/src/client.rs
+++ packages/hot-reload/src/client.rs
@@ -1,6 +1,7 @@
 use crate::HotReloadMsg;
 use dioxus_core::{ScopeId, VirtualDom};
 use dioxus_signals::Writable;
+use warnings::Warning;
 
 /// Applies template and literal changes to the VirtualDom
 ///
@@ -13,7 +14,11 @@ pub fn apply_changes(dom: &mut VirtualDom, msg: &HotReloadMsg) {
             let id = &template.location;
             let value = template.template.clone();
             if let Some(mut signal) = ctx.get_signal_with_key(id) {
-                signal.set(Some(value));
+                dioxus_signals::warnings::signal_read_and_write_in_reactive_scope::allow(|| {
+                    dioxus_signals::warnings::signal_write_in_component_body::allow(|| {
+                        signal.set(Some(value));
+                    });
+                });
             }
         }
     });
