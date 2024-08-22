294de5fbe92bb15cf57fd2c3441dc1e6bc3ffd26
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-08-22 02:18:28 -0700

    yeeeet that js out of here

--- packages/html/src/js/head.js
+++
@@ -1,1 +0,0 @@
-var createElementInHead=function(tag,attributes,children){const element=document.createElement(tag);for(let[key,value]of attributes)element.setAttribute(key,value);if(children)element.appendChild(document.createTextNode(children));document.head.appendChild(element)};window.createElementInHead=createElementInHead;
