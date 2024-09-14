46479550ac61b4efe4e30ee76069e42c725ef66d
Evan Almloff <evanalmloff@gmail.com>
2024-09-14 12:32:45 -0500

    update wifi scanner to 0.5

--- example-projects/wifi-scanner/src/main.rs
+++ example-projects/wifi-scanner/src/main.rs
@@ -1,30 +1,8 @@
 use dioxus::prelude::*;
-use dioxus_desktop::Config;
-use futures::StreamExt;
-use futures_channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
-use std::cell::Cell;
 use wifiscanner::Wifi;
 
 fn main() {
-    let (sender, receiver) = unbounded();
-
-    let other = sender.clone();
-
-    // launch our initial background scan in a thread
-    std::thread::spawn(move || {
-        let _ = other.unbounded_send(Status::Scanning);
-        let _ = other.unbounded_send(perform_scan());
-    });
-
-    // launch our app on the current thread - important because we spawn a window
-    dioxus_desktop::launch_with_props(
-        app,
-        AppProps {
-            sender: Cell::new(Some(sender)),
-            receiver: Cell::new(Some(receiver)),
-        },
-        Config::default(),
-    )
+    launch(app)
 }
 
 fn perform_scan() -> Status {
@@ -41,31 +19,15 @@ fn perform_scan() -> Status {
 
 enum Status {
     NoneFound,
-    Scanning,
     Found(Vec<Wifi>),
 }
 
-struct AppProps {
-    sender: Cell<Option<UnboundedSender<Status>>>,
-    receiver: Cell<Option<UnboundedReceiver<Status>>>,
-}
+fn app() -> Element {
+    let mut status =
+        use_resource(|| async { tokio::task::spawn_blocking(perform_scan).await.unwrap() });
+    let scanning = !status.finished();
 
-fn app(cx: Scope<AppProps>) -> Element {
-    let status = use_state(cx, || Status::NoneFound);
-
-    let _ = use_coroutine(cx, |_: UnboundedReceiver<()>| {
-        let receiver = cx.props.receiver.take();
-        let status = status.to_owned();
-        async move {
-            if let Some(mut receiver) = receiver {
-                while let Some(msg) = receiver.next().await {
-                    status.set(msg);
-                }
-            }
-        }
-    });
-
-    cx.render(rsx!(
+    rsx! {
         link { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" },
         div {
             div { class: "py-8 px-6",
@@ -73,19 +35,11 @@ fn app(cx: Scope<AppProps>) -> Element {
                     h2 { class: "text-2xl font-bold", "Scan for WiFi Networks" }
                     button {
                         class: "inline-block w-full md:w-auto px-6 py-3 font-medium text-white bg-indigo-500 hover:bg-indigo-600 rounded transition duration-200",
+                        disabled: scanning,
                         onclick: move |_| {
-                            let sender = cx.props.sender.take();
-                            std::thread::spawn( || {
-                            if let Some(sender) = sender {
-                                    let _ = sender.unbounded_send(Status::Scanning);
-                                    let _ = sender.unbounded_send(perform_scan());
-                                }
-                            });
+                            status.restart();
                         },
-                        match status.get() {
-                            Status::Scanning => rsx!("Scanning"),
-                            _ => rsx!("Scan"),
-                        }
+                        if scanning { "Scanning" } else { "Scan" }
                     }
                 }
             }
@@ -103,10 +57,10 @@ fn app(cx: Scope<AppProps>) -> Element {
                                 }
                             }
 
-                            match status.get() {
-                                Status::Scanning => rsx!(""),
-                                Status::NoneFound => rsx!("No networks found. Try scanning again"),
-                                Status::Found(wifis)  => {
+                            match &*status.read() {
+                                None => rsx!(""),
+                                Some(Status::NoneFound) => rsx!("No networks found. Try scanning again"),
+                                Some(Status::Found(wifis)) => {
                                     // Create vector of tuples of (signal_level, wifi) for sorting by signal_level
                                     let mut sorted_wifis = wifis
                                         .iter()
@@ -116,17 +70,14 @@ fn app(cx: Scope<AppProps>) -> Element {
 
                                     rsx! {
                                         tbody {
-                                            sorted_wifis.into_iter().rev().map(|(wi, _)|{
-                                                let Wifi { mac: _, ssid, channel, signal_level, security } = wi;
-                                                rsx!(
-                                                    tr { class: "text-xs bg-gray-50",
-                                                        td { class: "py-5 px-6 font-medium", "{signal_level}" }
-                                                        td { class: "flex py-3 font-medium", "{ssid}" }
-                                                        td { span { class: "inline-block py-1 px-2 text-white bg-green-500 rounded-full", "{channel}" } }
-                                                        td {  span { class: "inline-block py-1 px-2 text-purple-500 bg-purple-50 rounded-full", "{security}" } }
-                                                    }
-                                                )
-                                            })
+                                            for (Wifi { mac: _, ssid, channel, signal_level, security }, _) in sorted_wifis.into_iter().rev() {
+                                                tr { class: "text-xs bg-gray-50",
+                                                    td { class: "py-5 px-6 font-medium", "{signal_level}" }
+                                                    td { class: "flex py-3 font-medium", "{ssid}" }
+                                                    td { span { class: "inline-block py-1 px-2 text-white bg-green-500 rounded-full", "{channel}" } }
+                                                    td {  span { class: "inline-block py-1 px-2 text-purple-500 bg-purple-50 rounded-full", "{security}" } }
+                                                }
+                                            }
                                         }
                                     }
                                 }
@@ -136,5 +87,5 @@ fn app(cx: Scope<AppProps>) -> Element {
                 }
             }
         }
-    ))
+    }
 }
