146e8a00cc82dd7d58f48e37070c71d28f5c4738
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-09-15 16:41:32 -0700

    fix cargos referencing build stuff

--- packages/hot-reload/src/ws_receiver.rs
+++ packages/hot-reload/src/ws_receiver.rs
@@ -36,9 +36,10 @@ impl NativeReceiver {
 
     /// Connect to the devserver with an address from the CLI. Returns None if the current application was not run with the CLI
     pub async fn create_from_cli() -> Option<TtResult<Self>> {
-        let cli_args = dioxus_cli_config::RuntimeCLIArguments::from_cli()?;
-        let addr = cli_args.cli_address();
-        Some(Self::create(format!("ws://{addr}/_dioxus")).await)
+        todo!()
+        // let cli_args = dioxus_cli_config::
+        // let addr = cli_args.cli_address();
+        // Some(Self::create(format!("ws://{addr}/_dioxus")).await)
     }
 
     /// Wait for the next message from the devserver
