f0d814fedec1de489965fb9f2126b9fdb6ab92a9
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-09-16 16:04:20 -0700

    Extract some simple cleanups from 2779 (#2966)
    * Chore: remove random old test fixture from CLI
    
    * nuke oidc
    
    * remove weird useless desktop example
    
    * remove lua globals vscode setting for cli
    
    * remove cli testcase
    
    * hoist deps in core-macro
    
    * we dont need prettyplease
    
    * add hoisted dependencies
    
    * hoist more despt
    
    * rename rsx_rosetta to dioxus_rsx_rosetta
    
    * drop cache thrashing when listening for features
    
    * drop flag from mobile dep
    
    * drop cli-config warning
    
    * hoist more deps
    
    * clippy...
    
    * we dont use the tools module...
    
    * bump lock

--- packages/cli/src/cli/translate.rs
+++ packages/cli/src/cli/translate.rs
@@ -50,7 +50,7 @@ impl Translate {
 }
 
 pub fn convert_html_to_formatted_rsx(dom: &Dom, component: bool) -> String {
-    let callbody = rsx_rosetta::rsx_from_html(dom);
+    let callbody = dioxus_rsx_rosetta::rsx_from_html(dom);
 
     match component {
         true => write_callbody_with_icon_section(callbody),
@@ -61,7 +61,7 @@ pub fn convert_html_to_formatted_rsx(dom: &Dom, component: bool) -> String {
 fn write_callbody_with_icon_section(mut callbody: CallBody) -> String {
     let mut svgs = vec![];
 
-    rsx_rosetta::collect_svgs(&mut callbody.body.roots, &mut svgs);
+    dioxus_rsx_rosetta::collect_svgs(&mut callbody.body.roots, &mut svgs);
 
     let mut out = write_component_body(dioxus_autofmt::write_block_out(&callbody).unwrap());
 
@@ -129,13 +129,4 @@ fn determine_input(file: Option<String>, raw: Option<String>) -> Result<String> 
     std::io::stdin().read_to_string(&mut buffer).unwrap();
 
     Ok(buffer.trim().to_string())
-}
-
-#[test]
-fn generates_svgs() {
-    let st = include_str!("../../tests/svg.html");
-
-    let out = convert_html_to_formatted_rsx(&html_parser::Dom::parse(st).unwrap(), true);
-
-    println!("{}", out);
 }
