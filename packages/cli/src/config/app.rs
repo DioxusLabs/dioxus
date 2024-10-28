8a0eca1c8aa3e812f3899bc62f8ecc8c4154f819
Miles Murgaw <milesmurgaw@gmail.com>
2024-10-28 14:20:35 -0400

    remove: default_platform toml field (#3108)

--- packages/cli/src/config/app.rs
+++ packages/cli/src/config/app.rs
@@ -1,21 +1,13 @@
-use crate::Platform;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct ApplicationConfig {
-    #[serde(default = "default_platform")]
-    pub(crate) default_platform: Platform,
-
     #[serde(default = "asset_dir_default")]
     pub(crate) asset_dir: PathBuf,
 
     #[serde(default)]
     pub(crate) sub_package: Option<String>,
-}
-
-pub(crate) fn default_platform() -> Platform {
-    Platform::Web
 }
 
 pub(crate) fn asset_dir_default() -> PathBuf {
