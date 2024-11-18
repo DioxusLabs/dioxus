0a8726fcdef3df3bfdeba09bca8ccfad7b4a68fb
Evan Almloff <evanalmloff@gmail.com>
2024-11-18 10:49:42 -0600

    make hash pointer width independent

--- packages/manganis/src/hash.rs
+++ packages/manganis/src/hash.rs
@@ -1,13 +1,10 @@
 use const_serialize::{serialize_const, ConstVec, SerializeConst};
 
 // From rustchash -  https://github.com/rust-lang/rustc-hash/blob/6745258da00b7251bed4a8461871522d0231a9c7/src/lib.rs#L98
-#[cfg(target_pointer_width = "64")]
-const K: usize = 0xf1357aea2e62a9c5;
-#[cfg(target_pointer_width = "32")]
-const K: usize = 0x93d765dd;
+const K: u64 = 0xf1357aea2e62a9c5;
 
 pub(crate) struct ConstHasher {
-    hash: usize,
+    hash: u64,
 }
 
 impl ConstHasher {
@@ -16,7 +13,7 @@ impl ConstHasher {
     }
 
     pub const fn finish(&self) -> u64 {
-        self.hash as u64
+        self.hash
     }
 
     pub const fn write(mut self, bytes: &[u8]) -> Self {
@@ -29,7 +26,7 @@ impl ConstHasher {
     }
 
     pub const fn write_byte(mut self, byte: u8) -> Self {
-        self.hash = self.hash.wrapping_add(byte as usize).wrapping_mul(K);
+        self.hash = self.hash.wrapping_add(byte as u64).wrapping_mul(K);
         self
     }
 
