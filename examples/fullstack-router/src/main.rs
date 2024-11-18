06985f7ae6511d7b4bb2b199e05e21449be05b99
Evan Almloff <evanalmloff@gmail.com>
2024-11-18 12:30:10 -0600

    revert example change from clippy fix

--- examples/fullstack-router/src/main.rs
+++ examples/fullstack-router/src/main.rs
@@ -7,7 +7,12 @@
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus::LaunchBuilder::new().with_cfg(()).launch(app);
+    dioxus::LaunchBuilder::new()
+        .with_cfg(server_only!(ServeConfig::builder().incremental(
+            IncrementalRendererConfig::default()
+                .invalidate_after(std::time::Duration::from_secs(120)),
+        )))
+        .launch(app);
 }
 
 fn app() -> Element {
