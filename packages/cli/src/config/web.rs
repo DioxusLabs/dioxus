b114ed39f37c2a2a59b239a8541992569b0d90ef
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-12-02 13:34:14 -0800

    drop reference types due to deprecation, pin browserlist (fix ci) (#3272)
    * drop reference types due to deprecation
    
    * also downgrade browserlist

--- packages/cli/src/config/web.rs
+++ packages/cli/src/config/web.rs
@@ -58,10 +58,6 @@ pub(crate) struct WasmOptConfig {
     /// Keep debug symbols in the wasm file
     #[serde(default = "false_bool")]
     pub(crate) debug: bool,
-
-    /// Enable reference types
-    #[serde(default = "false_bool")]
-    pub(crate) reference_types: bool,
 }
 
 /// The wasm-opt level to use for release web builds [default: 4]
