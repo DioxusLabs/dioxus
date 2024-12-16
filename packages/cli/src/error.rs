c24a7ee89469d7d193961539b4352130f839a05f
Danilo Guanabara <danilo@magicblock.gg>
2024-12-16 00:31:34 -0300

    feature(cli): exposing Result

--- packages/cli/src/error.rs
+++ packages/cli/src/error.rs
@@ -1,7 +1,7 @@
 use crate::metadata::CargoError;
 use thiserror::Error as ThisError;
 
-pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[derive(ThisError, Debug)]
 pub enum Error {
