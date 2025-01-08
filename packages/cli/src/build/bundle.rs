c990c601b14ee031d47873fe92ca8305cae74aa0
Evan Almloff <evanalmloff@gmail.com>
2025-01-08 10:13:18 -0600

    move route prerendering into a separate stage

--- packages/cli/src/build/bundle.rs
+++ packages/cli/src/build/bundle.rs
@@ -1,3 +1,4 @@
+use super::prerender::pre_render_static_routes;
 use super::templates::InfoPlistData;
 use crate::wasm_bindgen::WasmBindgenBuilder;
 use crate::{BuildRequest, Platform};
@@ -283,6 +284,7 @@ impl AppBundle {
             .context("Failed to write assets")?;
         bundle.write_metadata().await?;
         bundle.optimize().await?;
+        bundle.pre_render_ssg_routes().await?;
         bundle
             .assemble()
             .await
@@ -556,11 +558,6 @@ impl AppBundle {
                 })
                 .await
                 .unwrap()?;
-
-                // Run SSG and cache static routes
-                if self.build.build.ssg {
-                    self.run_ssg().await?;
-                }
             }
             Platform::MacOS => {}
             Platform::Windows => {}
@@ -684,109 +681,18 @@ impl AppBundle {
         Ok(())
     }
 
-    async fn run_ssg(&self) -> anyhow::Result<()> {
-        use futures_util::stream::FuturesUnordered;
-        use futures_util::StreamExt;
-        use tokio::process::Command;
-
-        let fullstack_address = dioxus_cli_config::fullstack_address_or_localhost();
-        let address = fullstack_address.ip().to_string();
-        let port = fullstack_address.port().to_string();
-
-        tracing::info!("Running SSG");
-
-        // Run the server executable
-        let _child = Command::new(
-            self.server_exe()
+    async fn pre_render_ssg_routes(&self) -> Result<()> {
+        // Run SSG and cache static routes
+        if !self.build.build.ssg {
+            return Ok(());
+        }
+        self.build.status_prerendering_routes();
+        pre_render_static_routes(
+            &self
+                .server_exe()
                 .context("Failed to find server executable")?,
         )
-        .env(dioxus_cli_config::SERVER_PORT_ENV, port.clone())
-        .env(dioxus_cli_config::SERVER_IP_ENV, address.clone())
-        .stdout(std::process::Stdio::piped())
-        .stderr(std::process::Stdio::piped())
-        .kill_on_drop(true)
-        .spawn()?;
-
-        let reqwest_client = reqwest::Client::new();
-        // Borrow reqwest_client so we only move the reference into the futures
-        let reqwest_client = &reqwest_client;
-
-        // Get the routes from the `/static_routes` endpoint
-        let mut routes = None;
-
-        // The server may take a few seconds to start up. Try fetching the route up to 5 times with a one second delay
-        const RETRY_ATTEMPTS: usize = 5;
-        for i in 0..=RETRY_ATTEMPTS {
-            let request = reqwest_client
-                .post(format!("http://{address}:{port}/api/static_routes"))
-                .send()
-                .await;
-            match request {
-                Ok(request) => {
-                    routes = Some(request
-                        .json::<Vec<String>>()
-                        .await
-                        .inspect(|text| tracing::debug!("Got static routes: {text:?}"))
-                        .context("Failed to parse static routes from the server. Make sure your server function returns Vec<String> with the (default) json encoding")?);
-                    break;
-                }
-                Err(err) => {
-                    // If the request fails, try  up to 5 times with a one second delay
-                    // If it fails 5 times, return the error
-                    if i == RETRY_ATTEMPTS {
-                        return Err(err).context("Failed to get static routes from server. Make sure you have a server function at the `/api/static_routes` endpoint that returns Vec<String> of static routes.");
-                    }
-                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-                }
-            }
-        }
-
-        let routes = routes.expect(
-            "static routes should exist or an error should have been returned on the last attempt",
-        );
-
-        // Create a pool of futures that cache each route
-        let mut resolved_routes = routes
-            .into_iter()
-            .map(|route| async move {
-                tracing::info!("Rendering {route} for SSG");
-                // For each route, ping the server to force it to cache the response for ssg
-                let request = reqwest_client
-                    .get(format!("http://{address}:{port}{route}"))
-                    .header("Accept", "text/html")
-                    .send()
-                    .await?;
-                // If it takes longer than 30 seconds to resolve the route, log a warning
-                let warning_task = tokio::spawn({
-                    let route = route.clone();
-                    async move {
-                        tokio::time::sleep(Duration::from_secs(30)).await;
-                        tracing::warn!("Route {route} has been rendering for 30 seconds");
-                    }
-                });
-                // Wait for the streaming response to completely finish before continuing. We don't use the html it returns directly
-                // because it may contain artifacts of intermediate streaming steps while the page is loading. The SSG app should write
-                // the final clean HTML to the disk automatically after the request completes.
-                let _html = request.text().await?;
-
-                // Cancel the warning task if it hasn't already run
-                warning_task.abort();
-
-                Ok::<_, reqwest::Error>(route)
-            })
-            .collect::<FuturesUnordered<_>>();
-
-        while let Some(route) = resolved_routes.next().await {
-            match route {
-                Ok(route) => tracing::debug!("ssg success: {route:?}"),
-                Err(err) => tracing::error!("ssg error: {err:?}"),
-            }
-        }
-
-        tracing::info!("SSG complete");
-
-        drop(_child);
-
+        .await?;
         Ok(())
     }
 
