d535ab4f6ca98c2f6f55bfa2c7624d3e29f9c2be
Evan Almloff <evanalmloff@gmail.com>
2025-02-05 15:11:21 -0600

    revert fullstack desktop example changes

--- examples/fullstack-desktop/src/main.rs
+++ examples/fullstack-desktop/src/main.rs
@@ -3,6 +3,8 @@ use dioxus::prelude::*;
 
 fn main() {
     // Set the url of the server where server functions are hosted.
+    #[cfg(not(feature = "server"))]
+    dioxus::fullstack::prelude::server_fn::client::set_server_url("http://127.0.0.1:8080");
     dioxus::launch(app);
 }
 
