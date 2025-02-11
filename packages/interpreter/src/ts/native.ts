5d66d41a21a4881d351ed8868ed3107e88ba0792
Youn Mélois <youn@melois.dev>
2025-02-11 12:17:05 +0100

    fix(interpreter): return missing boolean from js

--- packages/interpreter/src/ts/native.ts
+++ packages/interpreter/src/ts/native.ts
@@ -115,11 +115,13 @@ export class NativeInterpreter extends JSChannel_ {
     return false;
   }
 
-  scroll(id: NodeId, x: number, y: number, behavior: ScrollBehavior) {
+  scroll(id: NodeId, x: number, y: number, behavior: ScrollBehavior): boolean {
     const node = this.nodes[id];
     if (node instanceof HTMLElement) {
       node.scroll({ top: y, left: x, behavior });
+      return true;
     }
+    return false;
   }
 
   getScrollHeight(id: NodeId): number | undefined {
