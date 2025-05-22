6fa78f0a76adab48023f5199685132371bfc117a
Evan Almloff <evanalmloff@gmail.com>
2025-05-22 13:06:52 -0500

    Fix setting option value to an empty string

--- packages/interpreter/src/ts/set_attribute.ts
+++ packages/interpreter/src/ts/set_attribute.ts
@@ -22,10 +22,16 @@ export function setAttributeInner(
   // A few attributes are need to be set with either boolean values or require some sort of translation
   switch (field) {
     case "value":
-      // @ts-ignore
-      if (node.value !== value) {
+      // If this is a option element, set the attribute normally
+      if (node.tagName === "OPTION") {
+        setAttributeDefault(node, field, value);
+      } else {
+        // Otherwise, set the property if it has changed
         // @ts-ignore
-        node.value = value;
+        if (node.value !== value) {
+          // @ts-ignore
+          node.value = value;
+        }
       }
       break;
 
