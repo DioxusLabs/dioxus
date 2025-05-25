42ff2aeb805cdfd392fa80bb333d190a11080dbb
Jonathan Kelley <jkelleyrtp@gmail.com>
2025-05-25 16:58:30 +0100

    watch the server

--- packages/server/src/lib.rs
+++ packages/server/src/lib.rs
@@ -80,7 +80,7 @@ pub use document::ServerDocument;
 mod launch;
 
 #[cfg(not(target_arch = "wasm32"))]
-pub use launch::launch;
+pub use launch::{launch_cfg, launch};
 
 /// Re-export commonly used items
 pub mod prelude {
