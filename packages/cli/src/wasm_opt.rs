8ea2c6aa42a6890db711d81fbde5b415c8b508a9
Evan Almloff <evanalmloff@gmail.com>
2025-06-03 12:51:27 -0500

    clean up and pass clippy

--- packages/cli/src/wasm_opt.rs
+++ packages/cli/src/wasm_opt.rs
@@ -15,16 +15,96 @@ pub async fn write_wasm(bytes: &[u8], output_path: &Path, cfg: &WasmOptConfig) -
 }
 
 pub async fn optimize(input_path: &Path, output_path: &Path, cfg: &WasmOptConfig) -> Result<()> {
-    let wasm_opt_path = get_binary_path().await.unwrap();
-    run_locally(&wasm_opt_path, input_path, output_path, cfg)
-        .await
-        .unwrap();
+    let wasm_opt = WasmOpt::new(input_path, output_path, cfg).await?;
+    wasm_opt.optimize().await?;
 
     Ok(())
 }
 
+struct WasmOpt {
+    path: PathBuf,
+    input_path: PathBuf,
+    output_path: PathBuf,
+    cfg: WasmOptConfig,
+}
+
+impl WasmOpt {
+    pub async fn new(
+        input_path: &Path,
+        output_path: &Path,
+        cfg: &WasmOptConfig,
+    ) -> anyhow::Result<Self> {
+        let path = get_binary_path().await?;
+        Ok(Self {
+            path,
+            input_path: input_path.to_path_buf(),
+            output_path: output_path.to_path_buf(),
+            cfg: cfg.clone(),
+        })
+    }
+
+    /// Create the command to run wasm-opt
+    fn build_command(&self) -> tokio::process::Command {
+        // defaults needed by wasm-opt.
+        // wasm is a moving target, and we add these by default since they progressively get enabled by default.
+        let mut args = vec![
+            "--enable-reference-types",
+            "--enable-bulk-memory",
+            "--enable-mutable-globals",
+            "--enable-nontrapping-float-to-int",
+        ];
+
+        if self.cfg.memory_packing {
+            // needed for our current approach to bundle splitting to work properly
+            // todo(jon): emit the main module's data section in chunks instead of all at once
+            args.push("--memory-packing");
+        }
+
+        if !self.cfg.debug {
+            args.push("--strip-debug");
+        } else {
+            args.push("--debuginfo");
+        }
+
+        for extra in &self.cfg.extra_features {
+            args.push(extra);
+        }
+
+        let level = match self.cfg.level {
+            WasmOptLevel::Z => "-Oz",
+            WasmOptLevel::S => "-Os",
+            WasmOptLevel::Zero => "-O0",
+            WasmOptLevel::One => "-O1",
+            WasmOptLevel::Two => "-O2",
+            WasmOptLevel::Three => "-O3",
+            WasmOptLevel::Four => "-O4",
+        };
+
+        let mut command = tokio::process::Command::new(&self.path);
+        command
+            .arg(&self.input_path)
+            .arg(level)
+            .arg("-o")
+            .arg(&self.output_path)
+            .args(args);
+        command
+    }
+
+    pub async fn optimize(&self) -> Result<()> {
+        let mut command = self.build_command();
+        let res = command.output().await?;
+
+        if !res.status.success() {
+            let err = String::from_utf8_lossy(&res.stderr);
+            tracing::error!("wasm-opt failed with status code {}: {}", res.status, err);
+        }
+
+        Ok(())
+    }
+}
+
+// Find the URL for the latest binaryen release that contains wasm-opt
 async fn find_latest_wasm_opt_download_url() -> anyhow::Result<String> {
-    // Find the latest release information from the GitHub API
     let url = "https://api.github.com/repos/WebAssembly/binaryen/releases/latest";
     let client = reqwest::Client::new();
     let response = client
@@ -38,6 +118,8 @@ async fn find_latest_wasm_opt_download_url() -> anyhow::Result<String> {
         .get("assets")
         .and_then(|assets| assets.as_array())
         .ok_or_else(|| anyhow::anyhow!("Failed to parse assets"))?;
+
+    // Find the platform identifier based on the current OS and architecture
     let platform = if cfg!(all(target_os = "windows", target_arch = "x86_64")) {
         "x86_64-windows"
     } else if cfg!(all(target_os = "linux", target_arch = "x86_64")) {
@@ -61,7 +143,7 @@ async fn find_latest_wasm_opt_download_url() -> anyhow::Result<String> {
             asset
                 .get("name")
                 .and_then(|name| name.as_str())
-                .map_or(false, |name| name.contains(platform))
+                .is_some_and(|name| name.contains(platform))
         })
         .ok_or_else(|| {
             anyhow::anyhow!(
@@ -70,6 +152,7 @@ async fn find_latest_wasm_opt_download_url() -> anyhow::Result<String> {
             )
         })?;
 
+    // Extract the download URL from the asset
     let download_url = asset
         .get("browser_download_url")
         .and_then(|url| url.as_str())
@@ -78,11 +161,16 @@ async fn find_latest_wasm_opt_download_url() -> anyhow::Result<String> {
     Ok(download_url.to_string())
 }
 
+/// Get the path to the wasm-opt binary, downloading it if necessary
 async fn get_binary_path() -> anyhow::Result<PathBuf> {
     let existing_path = which::which("wasm-opt");
+
     match existing_path {
+        // If wasm-opt is already in the PATH, return its path
         Ok(path) => Ok(path),
+        // If wasm-opt is not found in the path and we prefer no downloads, return an error
         Err(_) if CliSettings::prefer_no_downloads() => Err(anyhow!("Missing wasm-opt")),
+        // Otherwise, try to install it
         Err(_) => {
             let install_dir = install_dir().await?;
             let install_path = installed_bin_path(&install_dir);
@@ -115,102 +203,52 @@ fn installed_bin_path(install_dir: &Path) -> PathBuf {
     install_dir.join("bin").join(bin_name)
 }
 
+/// Install wasm-opt from GitHub releases into the specified directory
 async fn install_github(install_dir: &Path) -> anyhow::Result<()> {
     tracing::trace!("Attempting to install wasm-opt from GitHub");
 
-    let url = find_latest_wasm_opt_download_url().await?;
+    let url = find_latest_wasm_opt_download_url()
+        .await
+        .context("Failed to find latest wasm-opt download URL")?;
     tracing::trace!("Downloading wasm-opt from {}", url);
 
-    // Download then extract the latest wasm-opt binary
+    // Download the binaryen release archive into memory
     let bytes = reqwest::get(url).await?.bytes().await?;
 
-    let installed_bin_path = installed_bin_path(&install_dir);
-    // Unpack the dynamic library if it exists
+    // We don't need the whole gzip archive, just the wasm-opt binary and the lib folder. We
+    // just extract those files from the archive.
+    let installed_bin_path = installed_bin_path(install_dir);
     let lib_folder_name = "lib";
     let installed_lib_path = install_dir.join(lib_folder_name);
 
     // Create the lib and bin directories if they don't exist
-    for path in [installed_bin_path.parent(), Some(&installed_lib_path)] {
-        if let Some(path) = path {
-            std::fs::create_dir_all(path)
-                .context(format!("Failed to create directory: {}", path.display()))?;
-        }
+    for path in [installed_bin_path.parent(), Some(&installed_lib_path)]
+        .into_iter()
+        .flatten()
+    {
+        std::fs::create_dir_all(path)
+            .context(format!("Failed to create directory: {}", path.display()))?;
     }
 
     let mut archive = Archive::new(GzDecoder::new(bytes.as_ref()));
 
     // Unpack the binary and library files from the archive
     for mut entry in archive.entries()?.flatten() {
+        // Unpack the wasm-opt binary
         if entry
             .path_bytes()
             .ends_with(installed_bin_name().as_bytes())
         {
             entry.unpack(&installed_bin_path)?;
-        } else if let Ok(path) = entry.path() {
+        }
+        // Unpack any files in the lib folder
+        else if let Ok(path) = entry.path() {
             if path.components().any(|c| c.as_os_str() == lib_folder_name) {
                 if let Some(file_name) = path.file_name() {
-                    entry.unpack(&installed_lib_path.join(file_name))?;
+                    entry.unpack(installed_lib_path.join(file_name))?;
                 }
             }
         }
-    }
-
-    Ok(())
-}
-
-async fn run_locally(
-    wasm_opt_path: &Path,
-    input_path: &Path,
-    output_path: &Path,
-    cfg: &WasmOptConfig,
-) -> Result<()> {
-    // defaults needed by wasm-opt.
-    // wasm is a moving target, and we add these by default since they progressively get enabled by default.
-    let mut args = vec![
-        "--enable-reference-types",
-        "--enable-bulk-memory",
-        "--enable-mutable-globals",
-        "--enable-nontrapping-float-to-int",
-    ];
-
-    if cfg.memory_packing {
-        // needed for our current approach to bundle splitting to work properly
-        // todo(jon): emit the main module's data section in chunks instead of all at once
-        args.push("--memory-packing");
-    }
-
-    if !cfg.debug {
-        args.push("--strip-debug");
-    } else {
-        args.push("--debuginfo");
-    }
-
-    for extra in &cfg.extra_features {
-        args.push(extra);
-    }
-
-    let level = match cfg.level {
-        WasmOptLevel::Z => "-Oz",
-        WasmOptLevel::S => "-Os",
-        WasmOptLevel::Zero => "-O0",
-        WasmOptLevel::One => "-O1",
-        WasmOptLevel::Two => "-O2",
-        WasmOptLevel::Three => "-O3",
-        WasmOptLevel::Four => "-O4",
-    };
-
-    let res = tokio::process::Command::new(wasm_opt_path)
-        .arg(input_path)
-        .arg(level)
-        .arg("-o")
-        .arg(output_path)
-        .args(args)
-        .output()
-        .await?;
-
-    if !res.status.success() {
-        let err = String::from_utf8_lossy(&res.stderr);
-        tracing::error!("wasm-opt failed with status code {}: {}", res.status, err);
     }
 
     Ok(())
