8e727200823f415d5a198289e0f9dc624e2d9e28
mcmah309 <dillonmcmahon79@gmail.com>
2025-06-22 09:40:21 +0000

    Add comments to use_js macro example js

--- examples/assets/example.js
+++ examples/assets/example.js
@@ -1,7 +1,12 @@
+/** 
+ * This is a doc comment
+ * second line
+*/
 export function greeting(from, to) {
     return `Hello ${to}, this is ${from} speaking from JavaScript!`;
 }
 
+/// This is another doc comment
 export function add(a, b) {
     return a + b;
 }
