a3dc7047f383c7629d33a756adbf8567719eb0b4
Evan Almloff <evanalmloff@gmail.com>
2025-06-27 14:13:57 -0500

    record a playwright trace

--- packages/playwright-tests/playwright.config.js
+++ packages/playwright-tests/playwright.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     // Increase the timeout for navigations to give dx time to build the project
     navigationTimeout: 50 * 60 * 1000,
   },
