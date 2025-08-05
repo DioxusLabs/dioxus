7b3353de7aee1e1ffc13a182cb6458d3b87dad51
richerfu <southorange0929@foxmail.com>
2025-08-05 09:30:42 +0800

    fix: js crash

--- packages/interpreter/src/ts/native.ts
+++ packages/interpreter/src/ts/native.ts
@@ -505,7 +505,12 @@ function handleVirtualdomEventSync(
   xhr.send();
 
   // Deserialize the response, and then prevent the default/capture the event if the virtualdom wants to
-  return JSON.parse(xhr.responseText);
+  // @TODO Harmony crashed we need to fix this and remove this try/catch
+  try {
+    return JSON.parse(xhr.responseText);
+  } catch (error) {
+    console.error("Error deserializing event", error);
+  }
 }
 
 function getTargetId(target: EventTarget): NodeId | null {
