eec20b9bde53ebb70f8d5830728def8c39389f7d
Jonathan Kelley <jkelleyrtp@gmail.com>
2025-08-20 11:06:07 -0700

    fix merge conflicts / tests

--- packages/depinfo/src/lib.rs
+++ packages/depinfo/src/lib.rs
@@ -342,7 +342,6 @@ mod tests {
             "/dioxus/packages/html/src/attribute_groups.rs",
             "/dioxus/packages/html/src/elements.rs",
             "/dioxus/packages/html/src/events/animation.rs",
-            "/dioxus/packages/html/src/events/cancel.rs",
             "/dioxus/packages/html/src/events/clipboard.rs",
             "/dioxus/packages/html/src/events/composition.rs",
             "/dioxus/packages/html/src/events/drag.rs",
