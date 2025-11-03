5209abb3932f729329dbb022968a277ac8d0dca9
o137 <git-user@o137.net>
2025-11-04 01:20:10 +0900

    Fix local assets to be available without net features

--- packages/native/src/assets.rs
+++ packages/native/src/assets.rs
@@ -8,7 +8,7 @@ use winit::event_loop::EventLoopProxy;
 
 pub struct DioxusNativeNetProvider {
     callback: Arc<dyn NetCallback<Resource> + 'static>,
-    inner_net_provider: Arc<dyn NetProvider<Resource> + 'static>,
+    inner_net_provider: Option<Arc<dyn NetProvider<Resource> + 'static>>,
 }
 impl DioxusNativeNetProvider {
     pub fn shared(proxy: EventLoopProxy<BlitzShellEvent>) -> Arc<dyn NetProvider<Resource>> {
@@ -17,7 +17,12 @@ impl DioxusNativeNetProvider {
 
     pub fn new(proxy: EventLoopProxy<BlitzShellEvent>) -> Self {
         let net_callback = BlitzShellNetCallback::shared(proxy);
-        let net_provider = blitz_net::Provider::shared(net_callback.clone());
+
+        #[cfg(feature = "net")]
+        let net_provider = Some(blitz_net::Provider::shared(net_callback.clone()));
+        #[cfg(not(feature = "net"))]
+        let net_provider = None;
+
         Self {
             callback: net_callback,
             inner_net_provider: net_provider,
@@ -36,16 +41,21 @@ impl NetProvider<Resource> for DioxusNativeNetProvider {
             match dioxus_asset_resolver::native::serve_asset(request.url.path()) {
                 Ok(res) => {
                     #[cfg(feature = "tracing")]
-                    tracing::trace!("fetching asset  from file system success {request:#?}");
+                    tracing::trace!("fetching asset from file system success {request:#?}");
                     handler.bytes(doc_id, res.into_body().into(), self.callback.clone())
                 }
                 Err(_) => {
                     #[cfg(feature = "tracing")]
-                    tracing::warn!("fetching asset  from file system error {request:#?}");
+                    tracing::warn!("fetching asset from file system error {request:#?}");
                 }
             }
         } else {
-            self.inner_net_provider.fetch(doc_id, request, handler);
+            if let Some(inner_net_provider) = &self.inner_net_provider {
+                inner_net_provider.fetch(doc_id, request, handler);
+            } else {
+                #[cfg(feature = "tracing")]
+                tracing::warn!("net feature not enabled, cannot fetch {request:#?}");
+            }
         }
     }
 }
