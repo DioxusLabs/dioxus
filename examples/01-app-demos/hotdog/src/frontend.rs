54ef72397a1383a056141438eb3b0921295c8368
Evan Almloff <evanalmloff@gmail.com>
2025-12-26 07:45:44 -0600

    Use serde instead of json::Value

--- examples/01-app-demos/hotdog/src/frontend.rs
+++ examples/01-app-demos/hotdog/src/frontend.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     backend::{list_dogs, remove_dog, save_dog},
@@ -42,14 +43,15 @@ pub fn NavBar() -> Element {
 #[component]
 pub fn DogView() -> Element {
     let mut img_src = use_loader(|| async move {
+        #[derive(Deserialize, Serialize, Debug, PartialEq)]
+        struct DogApi {
+            message: String,
+        }
         let json = reqwest::get("https://dog.ceo/api/breeds/image/random")
             .await?
-            .json::<serde_json::Value>()
+            .json::<DogApi>()
             .await?;
-        let url = json["message"]
-            .as_str()
-            .expect("Failed to deserialize img URL")
-            .to_owned();
+        let url = json.message;
 
         dioxus::Ok(url)
     })?;
