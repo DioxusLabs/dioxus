996522094786e74083574e2202445149f6216c64
Henry <56412856+mcmah309@users.noreply.github.com>
2025-12-30 13:11:36 +0930

    Scoped css (#5087)
    * Create new css module parser
    
    * Use css module processing
    
    * Make hot reloading work for css_module macro
    
    * Change css_module macro shape and rename to styles
    
    * Add css_module example
    
    * Update styles macro documentation
    
    * Remove hidden from styles macro
    
    * Remove unneeded param
    
    * Remove unneeded visibility
    
    * Improve `styles!` macro documentation
    
    * Clippy
    
    * Remove unneeded method
    
    * Clippy
    
    * Clippy
    
    * Replace panic with anyhow
    
    * Update css_module macro to attribute macro
    
    * Update macro docs
    
    * chore: Cargo.lock
    
    * Clippy
    
    * respect semver by deprecating old func
    
    * update to latest winnow
    
    ---------
    
    Co-authored-by: Jonathan Kelley <jkelleyrtp@gmail.com>

--- packages/manganis/manganis-core/src/css_module.rs
+++ packages/manganis/manganis-core/src/css_module.rs
@@ -1,7 +1,12 @@
+use std::{
+    collections::HashSet,
+    hash::{DefaultHasher, Hash, Hasher},
+    path::Path,
+};
+
 use crate::{AssetOptions, AssetOptionsBuilder, AssetVariant};
 use const_serialize_07 as const_serialize;
 use const_serialize_08::SerializeConst;
-use std::collections::HashSet;
 
 /// Options for a css module asset
 #[derive(
@@ -94,6 +99,15 @@ impl AssetOptionsBuilder<CssModuleAssetOptions> {
     }
 }
 
+/// Create a hash for a css module based on the file path
+pub fn create_module_hash(css_path: &Path) -> String {
+    let path_string = css_path.to_string_lossy();
+    let mut hasher = DefaultHasher::new();
+    path_string.hash(&mut hasher);
+    let hash = hasher.finish();
+    format!("{:016x}", hash)[..8].to_string()
+}
+
 /// Collect CSS classes & ids.
 ///
 /// This is a rudementary css classes & ids collector.
@@ -102,6 +116,10 @@ impl AssetOptionsBuilder<CssModuleAssetOptions> {
 /// There are likely a number of edge cases that will show up.
 ///
 /// Returns `(HashSet<Classes>, HashSet<Ids>)`
+#[deprecated(
+    since = "0.7.3",
+    note = "This function is no longer used by the css module system and will be removed in a future release."
+)]
 pub fn collect_css_idents(css: &str) -> (HashSet<String>, HashSet<String>) {
     const ALLOWED: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-";
 
