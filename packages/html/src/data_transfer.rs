6870fd66ed3dbe49eed238ee67ebcf669dfd6baa
Kevin Wright <kev.lee.wright@gmail.com>
2026-01-10 23:33:59 +0100

    Suggested review changes

--- packages/html/src/data_transfer.rs
+++ packages/html/src/data_transfer.rs
@@ -1,8 +1,15 @@
+#[cfg(feature = "serialize")]
+pub use ser::*;
+#[cfg(feature = "serialize")]
+use serde::{Serialize, Deserialize};
+
 pub struct DataTransfer {
     inner: Box<dyn NativeDataTransfer>,
 }
 
 #[derive(Debug, PartialEq, Clone)]
+#[cfg(feature = "serialize")]
+#[derive(Serialize, Deserialize)]
 pub struct DataTransferItem {
     pub kind: String,
     pub type_: String,
@@ -74,7 +81,10 @@ impl DataTransfer {
 }
 
 pub trait NativeDataTransfer: Send + Sync {
-    fn items(&self) -> Vec<DataTransferItem>;
+    fn items(&self) -> Vec<DataTransferItem> {
+        //TODO: Good for back compatibility, but this can probably be lost for 0.8
+        Vec::new()
+    }
     fn get_data(&self, format: &str) -> Option<String>;
     fn set_data(&self, format: &str, data: &str) -> Result<(), String>;
     fn clear_data(&self, format: Option<&str>) -> Result<(), String>;
@@ -90,16 +100,14 @@ pub trait HasDataTransferData {
 }
 
 #[cfg(feature = "serialize")]
-pub use ser::*;
-#[cfg(feature = "serialize")]
-use serde::Serialize;
-
-#[cfg(feature = "serialize")]
 mod ser {
     use crate::DragData;
 
     use super::*;
     use serde::{Deserialize, Serialize};
+
+    type SerializedDataTransferItem = DataTransferItem;
+
 
     /// A serialized version of DataTransfer
     #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
@@ -110,23 +118,9 @@ mod ser {
         pub drop_effect: String,
     }
 
-    #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-    pub struct SerializedDataTransferItem {
-        pub kind: String,
-        pub type_: String,
-        pub data: String,
-    }
-
     impl NativeDataTransfer for SerializedDataTransfer {
         fn items(&self) -> Vec<DataTransferItem> {
-            self.items
-                .iter()
-                .map(|item| DataTransferItem{
-                    kind: item.kind.clone(),
-                    type_: item.type_.clone(),
-                    data: item.data.clone(),
-                })
-                .collect::<Vec<_>>()
+            self.items.clone()
         }
 
         fn get_data(&self, format: &str) -> Option<String> {
