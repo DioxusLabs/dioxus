73a0237178b5203c034230a8164157b80828dacc
Kevin Wright <kev.lee.wright@gmail.com>
2026-01-11 03:00:34 +0100

    Dropped-file data is now serialised as `data:` URL rather than a `blob:` URL.
    The downside is that base64 encoding is less efficient and there's potentially a limit to the size of javascript strings. The upshot is that the resulting URL is now actually usable in rust code and not only within the webview.

--- packages/interpreter/src/ts/serialize.ts
+++ packages/interpreter/src/ts/serialize.ts
@@ -12,10 +12,20 @@ export type SerializedEvent = {
   [key: string]: any;
 };
 
-export function serializeEvent(
+async function blobToDataURL(blob: Blob): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = _e => resolve(reader.result as string);
+    reader.onerror = _e => reject(reader.error);
+    reader.onabort = _e => reject(new Error("Read aborted"));
+    reader.readAsDataURL(blob);
+  });
+}
+
+export async function serializeEvent(
   event: Event,
   target: EventTarget
-): SerializedEvent {
+): Promise<SerializedEvent> {
   let contents = {};
 
   // merge the object into the contents
@@ -51,7 +61,7 @@ export function serializeEvent(
     extend({ data: event.data });
   }
   if (event instanceof DragEvent) {
-    extend(serializeDragEvent(event));
+    extend(await serializeDragEvent(event));
   }
   if (event instanceof FocusEvent) {
     extend({});
@@ -95,7 +105,7 @@ export function serializeEvent(
           size: file.size,
           last_modified: file.lastModified,
           content_type: file.type,
-          url: URL.createObjectURL(file),
+          url: await blobToDataURL(file),
         }
         files.push({ key: file.name, file: data });
       }
@@ -321,7 +331,7 @@ function serializeAnimationEvent(event: AnimationEvent): SerializedEvent {
   };
 }
 
-function serializeDragEvent(event: DragEvent): SerializedEvent {
+async function serializeDragEvent(event: DragEvent): Promise<SerializedEvent> {
   let data_transfer = event.dataTransfer || new DataTransfer();
   let items = [];
   let files = [];
@@ -355,7 +365,7 @@ function serializeDragEvent(event: DragEvent): SerializedEvent {
       last_modified: file.lastModified,
       content_type: file.type,
       contents: undefined, // we don't serialize contents here
-      url: URL.createObjectURL(file),
+      url: await blobToDataURL(file),
     });
   }
 
@@ -469,7 +479,6 @@ function retrieveFormValues(form: HTMLFormElement): SerializedFormData {
         size: value.size,
         last_modified: value.lastModified,
         content_type: value.type,
-        url: URL.createObjectURL(value),
       };
       contents.push({ key, file: fileData });
     } else {
