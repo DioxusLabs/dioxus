e1a595593c047c1bf0d48a40f5ba4d060e8a5859
FCG <develop.fcg@gmail.com>
2026-01-18 20:14:11 +0900

    -

--- examples/10-integrations/pwa/public/sw.js
+++ examples/10-integrations/pwa/public/sw.js
@@ -5,7 +5,7 @@
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var version = 'v1.0.0::';
+var version = 'v1.0.0-rc1::';
 
 /* These resources will be downloaded and cached by the service worker
    during the installation process. If any resource fails to be downloaded,
