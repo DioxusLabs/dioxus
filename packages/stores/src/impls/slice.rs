35339450a36ac757aee093d2cb579cccc5372285
Evan Almloff <evanalmloff@gmail.com>
2026-02-13 10:56:56 -0600

    fix removing keyed items from a vec store

--- packages/stores/src/impls/slice.rs
+++ packages/stores/src/impls/slice.rs
@@ -1,7 +1,13 @@
-use std::iter::FusedIterator;
+//! Additional utilities for `Vec` stores.
 
-use crate::{impls::index::IndexWrite, store::Store};
-use dioxus_signals::{Readable, ReadableExt};
+use std::{iter::FusedIterator, panic::Location};
+
+use crate::{impls::index::IndexSelector, store::Store, ReadStore};
+use dioxus_signals::{
+    AnyStorage, BorrowError, BorrowMutError, ReadSignal, Readable, ReadableExt, UnsyncStorage,
+    Writable, WriteLock, WriteSignal,
+};
+use generational_box::ValueDroppedError;
 
 impl<Lens, I> Store<Vec<I>, Lens>
 where
@@ -46,16 +52,18 @@ where
     ///     println!("{}", item);
     /// }
     /// ```
+    #[track_caller]
     pub fn iter(
         &self,
-    ) -> impl ExactSizeIterator<Item = Store<I, IndexWrite<usize, Lens>>>
+    ) -> impl ExactSizeIterator<Item = Store<I, VecGetWrite<Lens>>>
            + DoubleEndedIterator
            + FusedIterator
            + '_
     where
         Lens: Clone,
     {
-        (0..self.len()).map(move |i| self.clone().index(i))
+        let location = Location::caller();
+        (0..self.len()).map(move |i| self.clone().get_unchecked_at(i, location))
     }
 
     /// Try to get an item from slice. This will only track the shallow state of the slice.
@@ -70,14 +78,130 @@ where
     /// // The indexed store can access the store methods of the indexed store.
     /// assert_eq!(indexed_store(), 2);
     /// ```
-    pub fn get(&self, index: usize) -> Option<Store<I, IndexWrite<usize, Lens>>>
+    pub fn get(&self, index: usize) -> Option<Store<I, VecGetWrite<Lens>>>
     where
         Lens: Clone,
     {
         if index >= self.len() {
             None
         } else {
-            Some(self.clone().index(index))
+            Some(self.clone().get_unchecked(index))
         }
+    }
+
+    /// Get a store for the item at the given index without checking if it is in bounds.
+    ///
+    /// This is not unsafe, but reads will return a [BorrowError::Dropped] error if the index is out of bounds.
+    #[track_caller]
+    pub fn get_unchecked(self, index: usize) -> Store<I, VecGetWrite<Lens>> {
+        self.get_unchecked_at(index, Location::caller())
+    }
+
+    fn get_unchecked_at(
+        self,
+        index: usize,
+        location: &'static Location<'static>,
+    ) -> Store<I, VecGetWrite<Lens>> {
+        <Vec<I>>::scope_selector(self.into_selector(), &index)
+            .map_writer(move |write| VecGetWrite {
+                index,
+                write,
+                created: location,
+            })
+            .into()
+    }
+}
+
+/// A specific index in a `Readable` / `Writable` Vec that uses safe `.get()` / `.get_mut()` access.
+#[derive(Clone, Copy)]
+pub struct VecGetWrite<Write> {
+    index: usize,
+    write: Write,
+    created: &'static Location<'static>,
+}
+
+impl<Write, T> Readable for VecGetWrite<Write>
+where
+    Write: Readable<Target = Vec<T>>,
+    T: 'static,
+{
+    type Target = T;
+    type Storage = Write::Storage;
+
+    fn try_read_unchecked(&self) -> Result<dioxus_signals::ReadableRef<'static, Self>, BorrowError>
+    where
+        Self::Target: 'static,
+    {
+        self.write.try_read_unchecked().and_then(|value| {
+            let index = self.index;
+            Self::Storage::try_map(value, move |value: &Vec<T>| value.get(index))
+                .ok_or_else(|| BorrowError::Dropped(ValueDroppedError::new(self.created)))
+        })
+    }
+
+    fn try_peek_unchecked(&self) -> Result<dioxus_signals::ReadableRef<'static, Self>, BorrowError>
+    where
+        Self::Target: 'static,
+    {
+        self.write.try_peek_unchecked().and_then(|value| {
+            let index = self.index;
+            Self::Storage::try_map(value, move |value: &Vec<T>| value.get(index))
+                .ok_or_else(|| BorrowError::Dropped(ValueDroppedError::new(self.created)))
+        })
+    }
+
+    fn subscribers(&self) -> dioxus_core::Subscribers
+    where
+        Self::Target: 'static,
+    {
+        self.write.subscribers()
+    }
+}
+
+impl<Write, T> Writable for VecGetWrite<Write>
+where
+    Write: Writable<Target = Vec<T>>,
+    T: 'static,
+{
+    type WriteMetadata = Write::WriteMetadata;
+
+    fn try_write_unchecked(
+        &self,
+    ) -> Result<dioxus_signals::WritableRef<'static, Self>, BorrowMutError>
+    where
+        Self::Target: 'static,
+    {
+        self.write.try_write_unchecked().and_then(|value| {
+            let index = self.index;
+            WriteLock::filter_map(value, move |value: &mut Vec<T>| value.get_mut(index))
+                .ok_or_else(|| BorrowMutError::Dropped(ValueDroppedError::new(self.created)))
+        })
+    }
+}
+
+impl<T, Write> ::std::convert::From<Store<T, VecGetWrite<Write>>> for Store<T, WriteSignal<T>>
+where
+    Write: Writable<Target = Vec<T>, Storage = UnsyncStorage> + 'static,
+    Write::WriteMetadata: 'static,
+    T: 'static,
+{
+    fn from(value: Store<T, VecGetWrite<Write>>) -> Self {
+        value
+            .into_selector()
+            .map_writer(|writer| WriteSignal::new(writer))
+            .into()
+    }
+}
+
+impl<T, Write> ::std::convert::From<Store<T, VecGetWrite<Write>>> for ReadStore<T>
+where
+    Write: Readable<Target = Vec<T>, Storage = UnsyncStorage> + 'static,
+    T: 'static,
+{
+    fn from(value: Store<T, VecGetWrite<Write>>) -> Self {
+        value
+            .into_selector()
+            .map_writer(|writer| ReadSignal::new(writer))
+            .into()
     }
 }
