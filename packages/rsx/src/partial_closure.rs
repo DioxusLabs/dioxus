0312ee11793d62fde85be0320660688f116d2c22
Evan Almloff <evanalmloff@gmail.com>
2026-03-10 11:01:23 -0700

    fix a few ci failures

--- packages/rsx/src/partial_closure.rs
+++ packages/rsx/src/partial_closure.rs
@@ -11,7 +11,7 @@ use syn::{BoundLifetimes, ExprClosure};
 
 /// A closure whose body might not be valid rust code but we want to interpret it regardless.
 /// This lets us provide expansions in way more cases than normal closures at the expense of an
-/// increased mainteance burden and complexity.
+/// increased maintenance burden and complexity.
 ///
 /// We do our best to reuse the same logic from partial exprs for the body of the PartialClosure.
 /// The code here is simply stolen from `syn::ExprClosure` and lightly modified to work with
