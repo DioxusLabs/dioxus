232a8dc83dc0a999dc9a0b4c4fc4b5f5bb93d6d8
Javier Su <javier.su.weijie@gmail.com>
2026-03-27 14:10:08 +0800

    fix: additional new line in info.plist (#5389)
    Co-authored-by: Jonathan Kelley <jkelleyrtp@gmail.com>

--- packages/cli/src/build/request.rs
+++ packages/cli/src/build/request.rs
@@ -7066,8 +7066,7 @@ We checked the folders:
         };
 
         let entitlements_xml = format!(
-            r#"
-<?xml version="1.0" encoding="UTF-8"?>
+            r#"<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0"><dict>
     <key>application-identifier</key>
