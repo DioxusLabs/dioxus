3cebd6942666ddf9740a4298f19fa64ecb5830f3
Evan Almloff <evanalmloff@gmail.com>
2026-04-14 10:14:07 -0500

    wip magic

--- examples/01-app-demos/repo_readme.rs
+++ examples/01-app-demos/repo_readme.rs
@@ -14,9 +14,50 @@ fn main() {
 fn app() -> Element {
     let mut count = use_signal(|| 0);
 
+    // fails because private is not in scope
+    <W as A>::private::A();
+
     rsx! {
         h1 { "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
     }
+}
+
+pub enum MyPhantom<T> {
+    A,
+    B(T),
+}
+
+mod private {
+    use crate::{A, MyPhantom, W};
+    use std::ops::Deref;
+
+    // Works because private is in scope
+    fn test() {
+        <W as A>::private::A();
+    }
+
+    struct Private;
+
+    impl Deref for MyPhantom<Private> {
+        type Target = fn();
+
+        fn deref(&self) -> &Self::Target {
+            fn f() {
+                println!("Hello from the private function!");
+            }
+            &(f as fn())
+        }
+    }
+}
+
+pub struct W;
+
+pub trait A {
+    type private<T>;
+}
+
+impl A for W {
+    type private<T> = MyPhantom<T>;
 }
