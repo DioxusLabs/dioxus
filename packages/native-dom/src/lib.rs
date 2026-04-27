1766af46bd23dc7fcbda10d530b14e046a20c95e
Nico Burns <nico@nicoburns.com>
2026-04-27 18:23:46 +0100

    Sync 0.8.x versions of `dioxus-native` and `dioxus-native-dom` from Blitz repo (#5507)
    * Sync native and native-dom packages from blitz repo (0.8)
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    * Fixups
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    * Use Rust 1.92 in CI
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    * Fix compiling dioxus-native with --all-features
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    * Bump bytemuck dep to 1.25
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>
    
    * Fixup minimal versions
    
    ---------
    
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- packages/native-dom/src/lib.rs
+++ packages/native-dom/src/lib.rs
@@ -12,8 +12,11 @@
 mod dioxus_document;
 mod events;
 mod mutation_writer;
+mod sub_document;
 pub use blitz_dom::DocumentConfig;
 pub use dioxus_document::DioxusDocument;
+pub use events::NodeHandle;
+pub use sub_document::SubDocumentAttr;
 
 use blitz_dom::{LocalName, Namespace, QualName, ns};
 type NodeId = usize;
