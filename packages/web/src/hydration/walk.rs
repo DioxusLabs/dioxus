8e4b1ec85e2503eb77877f50c56704a401d19380
Evan Almloff <evanalmloff@gmail.com>
2026-06-24 20:55:31 -0500

    fix web hydration

--- packages/web/src/hydration/walk.rs
+++ packages/web/src/hydration/walk.rs
@@ -243,7 +243,8 @@ fn mounted_static_node<'a>(
     dom: &'a VirtualDom,
 ) -> Option<ElementId> {
     root_anchor_idx
-        .and_then(|anchor_idx| vnode.mounted_static_anchor(anchor_idx, dom))
+        .and_then(|anchor_idx| dom.mounted_anchor_node(vnode.mount(), anchor_idx))
+        .map(|id| id.element_id())
         .or(pending_before_anchor)
 }
 
